650164fdba20b0b11b8aea2f6a9a2fe610756d9d
Saeid Khajevand <saeid.kh7@gmail.com>
2025-10-05 20:13:34 -0400

    Add password hashing for admin user in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +19,7 @@ class DatabaseSeeder extends Seeder
         'lastname' => env('APP_NAME', 'App'),
         'username' => 'admin',
         'email' => env('ADMIN_EMAIL', 'admin@example.com'),
+        'password' => Hash::make('12345678'),
       ]);
 
       User::factory(10)->create();
