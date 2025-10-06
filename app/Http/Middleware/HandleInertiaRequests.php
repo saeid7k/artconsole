f276cddf1a1f301d9ca5207423c00855b9244683
Saeid Khajevand <saeid.kh7@gmail.com>
2025-10-05 20:15:44 -0400

    add current gallery to inertia request

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -8,37 +8,41 @@ use Inertia\Middleware;
 
 class HandleInertiaRequests extends Middleware
 {
-    /**
-     * The root template that is loaded on the first page visit.
-     *
-     * @var string
-     */
-    protected $rootView = 'app';
+  /**
+   * The root template that is loaded on the first page visit.
+   *
+   * @var string
+   */
+  protected $rootView = 'app';
 
-    /**
-     * Determine the current asset version.
-     */
-    public function version(Request $request): ?string
-    {
-        return parent::version($request);
-    }
+  /**
+   * Determine the current asset version.
+   */
+  public function version(Request $request): ?string
+  {
+    return parent::version($request);
+  }
 
-    /**
-     * Define the props that are shared by default.
-     *
-     * @return array<string, mixed>
-     */
-    public function share(Request $request): array
-    {
-        return [
-            ...parent::share($request),
-            'auth' => [
-                'user' => $request->user(),
-            ],
-            'flash' => [
-              'type' => Session::get('flash.type'),
-              'message' => Session::get('flash.message'),
-            ],
-        ];
-    }
+  /**
+   * Define the props that are shared by default.
+   *
+   * @return array<string, mixed>
+   */
+  public function share(Request $request): array
+  {
+    $user = $request->user();
+    $gallery = $user?->currentGallery() ?? null;
+
+    return [
+      ...parent::share($request),
+      'auth' => [
+        'user' => $user,
+      ],
+      'gallery' => $gallery,
+      'flash' => [
+        'type' => Session::get('flash.type'),
+        'message' => Session::get('flash.message'),
+      ],
+    ];
+  }
 }
