6c0e742cfb2484aaa1c2d5de2f776efe0fe1065f
Saeid Khajevand <saeid.kh7@gmail.com>
2025-10-11 00:01:07 -0400

    develop contact model and add index controller

--- app/Helpers/AddressHelper.php
+++ app/Helpers/AddressHelper.php
@@ -10,4 +10,13 @@ class AddressHelper
     $provincesData = json_decode($provincesJson, true);
     return array_keys($provincesData['Canada']['provinces']);
   }
+
+  public static function formatPostalCode(string $postalCode): string
+  {
+    $postalCode = strtoupper(str_replace(' ', '', $postalCode));
+    if (strlen($postalCode) === 6) {
+      return substr($postalCode, 0, 3) . ' ' . substr($postalCode, 3);
+    }
+    return $postalCode;
+  }
 }
--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,25 +2,41 @@
 
 namespace App\Models;
 
+use App\Helpers\AddressHelper;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
   use HasFactory;
-  
+
   protected $casts = [
     'address' => 'object',
     'business' => 'object',
     'birthday' => 'date',
   ];
 
-  protected $appends = ['full_name'];
+  // Appends
+
+  protected $appends = ['full_name', 'formatted_address'];
 
   public function getFullNameAttribute(): string
   {
     return trim($this->firstname . ' ' . $this->lastname);
   }
+
+  public function getFormattedAddressAttribute(): string
+  {
+    $address = $this->address->unit ? $this->address->street . ' - ' . $this->address->unit : $this->address->street;
+    $address .= $this->address->city ? ', ' . $this->address->city : '';
+    $address .= $this->address->province ? ', ' . $this->address->province : '';
+    $address .= $this->address->postal_code ? ' ' . AddressHelper::formatPostalCode($this->address->postal_code) : '';
+    $address .= $this->address->country ? ', ' . $this->address->country : '';
+
+    return $address;
+  }
+
+  // Relationships
 
   public function owner()
   {
