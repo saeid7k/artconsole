d7ba7c04e5096806a190731ce9d4dc325c0e481d
Saeid Khajevand <saeid.kh7@gmail.com>
2025-10-12 18:16:52 -0400

    refactor ContactStack component to use getInitials function and update class name from .light-small to .text-ghost

--- resources/js/Components/ContactStack.tsx
+++ resources/js/Components/ContactStack.tsx
@@ -1,21 +1,21 @@
 import { Contact } from "@/types/contact";
 import { stringToColor } from "@/utils/colorHelper";
+import { getInitials } from "@/utils/stringHelper";
 import { Avatar } from "antd";
 
 function ContactStack({contact}: {contact: Contact}) {
 
-  const initials = `${contact.firstname.charAt(0)}${contact.lastname.charAt(0)}`
   return (
     <div className="flex items-center gap-2">
       <Avatar
         className="tracking-wider min-w-8"
-        style={{ backgroundColor: stringToColor(initials) }}
+        style={{ backgroundColor: stringToColor(getInitials(contact.full_name)) }}
       >
-        {initials}
+        {getInitials(contact.full_name)}
       </Avatar>
       <div>
         <div className="leading-tight">{contact.full_name}</div>
-        <div className="light-small leading-tight">{contact.business?.name}</div>
+        <div className="text-ghost leading-tight">{contact.business?.name}</div>
       </div>
     </div>
   );
