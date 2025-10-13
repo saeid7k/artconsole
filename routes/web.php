800f6c7becdfd06403a9f1c2a753e91b1085516f
Saeid Khajevand <saeid.kh7@gmail.com>
2025-10-12 21:46:11 -0400

    add gallery switching functionality

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth', 'verified'])->group(function () {
   Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
   Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+
+  Route::prefix('galleries')->name('galleries.')->group(function () {
+    Route::post('set-current', [App\Http\Controllers\GalleryController::class, 'setCurrentGallery'])->name('set-current');
+  });
 });
 
 require __DIR__.'/auth.php';
