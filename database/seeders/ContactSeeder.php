cf30ca22639dede15fe134dc1da04b482d6c40e9
Saeid Khajevand <saeid.kh7@gmail.com>
2025-10-12 23:11:10 -0400

    update ContactSeeder to create multiple contacts for each gallery

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -13,5 +13,12 @@ class ContactSeeder extends Seeder
     Contact::factory(50)->create([
       'gallery_id' => Gallery::first()->id,
     ]);
+
+    $galleries = Gallery::all();
+    foreach ($galleries as $gallery) {
+      Contact::factory(5)->create([
+        'gallery_id' => $gallery->id,
+      ]);
+    }
   }
 }
