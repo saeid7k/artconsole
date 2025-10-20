2966447de35cf1ae2ac8c723d84f766501f33b4f
Saeid Khajevand <saeid.kh7@gmail.com>
2025-10-19 20:12:47 -0400

    implement user deletion confirmation with axios and message feedback

--- resources/js/Pages/Users/Partials/UsersActions.tsx
+++ resources/js/Pages/Users/Partials/UsersActions.tsx
@@ -2,44 +2,65 @@ import { UserProps } from "@/types/user"
 import { Delete02Icon, Login01Icon, SidebarRightIcon, ViewIcon } from "@hugeicons/core-free-icons"
 import { HugeiconsIcon } from "@hugeicons/react"
 import { router } from "@inertiajs/react"
-import { Button, Tooltip } from "antd"
+import { Button, message, Popconfirm, Tooltip } from "antd"
+import axios from "axios"
 
-function UsersActions({ user }: { user: UserProps })
-{
-    return (
-        <div
-          className="flex items-center gap-1"
+function UsersActions({ user }: { user: UserProps }) {
+
+  function confirmDelete() {
+    axios.post(route('users.delete', user.id))
+      .then((res) => {
+        message.success(res.data.message || 'User deleted successfully')
+        router.reload()
+      })
+      .catch((e) => {
+        message.error(e.response?.data?.message || 'An error occurred while deleting the user')
+      })
+  }
+
+  return (
+    <div
+      className="flex items-center gap-1"
+    >
+      <Tooltip title="Login as User">
+        <Button
+          variant="text"
+          color='purple'
+          shape="circle"
+          icon={<HugeiconsIcon icon={Login01Icon} size={20} />}
+          onClick={() => router.visit(route('login-as', user.id))}
+        />
+      </Tooltip>
+      <Tooltip title="View Details">
+        <Button
+          variant="text"
+          color='blue'
+          shape="circle"
+          icon={<HugeiconsIcon icon={SidebarRightIcon} size={20} />}
+          onClick={() => router.visit(route('user.show', user.id))}
+        />
+      </Tooltip>
+      <Tooltip title="Delete">
+        <Popconfirm
+          title="Delete the user"
+          description="Are you sure to delete this user?"
+          onConfirm={() => confirmDelete()}
+          okText="Yes"
+          cancelText="No"
+          placement="left"
+          okType="danger"
         >
-          <Tooltip title="Login as User">
-            <Button
-              variant="text"
-              color='purple'
-              shape="circle"
-              icon={<HugeiconsIcon icon={Login01Icon} size={20} />}
-              onClick={() => router.visit(route('login-as', user.id))}
-            />
-          </Tooltip>
-          <Tooltip title="View Details">
-            <Button
-              variant="text"
-              color='blue'
-              shape="circle"
-              icon={<HugeiconsIcon icon={SidebarRightIcon} size={20} />}
-              onClick={() => router.visit(route('user.show', user.id))}
-            />
-          </Tooltip>
-          <Tooltip title="Delete">
-            <Button
-              variant="text"
-              color='danger'
-              shape="circle"
-              icon={<HugeiconsIcon icon={Delete02Icon} size={20} />}
-              onClick={() => router.post(route('users.remove', user.id))}
-              // disabled={!user.abilities.delete}
-            />
-          </Tooltip>
-        </div>
-    )
+          <Button
+            variant="text"
+            color='danger'
+            shape="circle"
+            icon={<HugeiconsIcon icon={Delete02Icon} size={20} />}
+            disabled={!user.abilities.delete}
+          />
+        </Popconfirm>
+      </Tooltip>
+    </div>
+  )
 }
 
 export default UsersActions
