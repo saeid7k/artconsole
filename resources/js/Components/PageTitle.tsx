a9d31ddd03b80758361bcbf6142b000e767ea96b
Saeid Khajevand <saeid.kh7@gmail.com>
2025-10-27 01:00:51 -0400

    add breadcrumb prop to PageTitle component

--- resources/js/Components/PageTitle.tsx
+++ resources/js/Components/PageTitle.tsx
@@ -1,17 +1,30 @@
+import { Breadcrumb } from "antd";
+
 type Props = {
-  title: string | null;
+  title?: string | null;
   counter?: number | null;
   toolbar?: React.ReactNode | null;
+  breadcrumbItems?: {
+    title: string | React.ReactNode;
+  }[];
 }
 
-function PageTitle({ title = null, counter = null, toolbar = null }: Props) {
+function PageTitle({ title = null, counter = null, toolbar = null, breadcrumbItems = [] }: Props) {
   return (
       <div
         className="flex items-center justify-between px-6 pb-3 w-full grow-0"
       >
-        <div className="flex items-center gap-2">
-          <h3 className="m-0">{title}</h3>
-          {counter && <small className="text-muted font-light">({counter.toLocaleString()})</small>}
+        <div>
+          {breadcrumbItems.length > 0 && (
+            <Breadcrumb
+              items={breadcrumbItems}
+              className="mb-1"
+            />
+          )}
+          <div className="flex items-center gap-2">
+            {title && <h3 className="m-0">{title}</h3>}
+            {counter && <small className="text-muted font-light">({counter.toLocaleString()})</small>}
+          </div>
         </div>
         {toolbar && <div>{toolbar}</div>}
       </div>
