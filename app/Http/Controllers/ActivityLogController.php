1394123f386b0cb73817f5f2aff3651a6e58c2bd
Saeid Khajevand <saeid.kh7@gmail.com>
2025-10-31 01:24:09 -0400

    add ActivityLogController and route for model activities

---
+++ app/Http/Controllers/ActivityLogController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Activitylog\Models\Activity;
+
+class ActivityLogController extends Controller
+{
+  public function modelActivities(Request $request) {
+    $model = ucfirst($request->input('model_type'));
+    $modelId = $request->input('model_id');
+
+    $activities = Activity::where('subject_type', 'App\\Models\\' . $model)
+      ->where('subject_id', $modelId)
+      ->with('causer')
+      ->orderBy('created_at', 'desc')
+      ->get();
+
+    return response()->json($activities);
+  }
+}
