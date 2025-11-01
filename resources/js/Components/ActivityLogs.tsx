4e30581fbb774083d62313b6b144116101d422a5
Saeid Khajevand <saeid.kh7@gmail.com>
2025-10-31 23:50:38 -0400

    add loading and empty states to ActivityLogs component

--- resources/js/Components/ActivityLogs.tsx
+++ resources/js/Components/ActivityLogs.tsx
@@ -1,28 +1,44 @@
 import { ActivityLogProps } from "@/types/activityLog";
 import { Time04Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { Avatar } from "antd";
+import { Avatar, Empty, Spin } from "antd";
 import axios from "axios";
 import moment from "moment";
 import { useEffect, useState } from "react";
+import LoadingSpinner from "@/Components/LoadingSpinner";
 
 function ActivityLogs({ modelType, modelId }: { modelType: string; modelId: number }) {
 
   const [logs, setLogs] = useState<Array<ActivityLogProps>>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   function fetchActivityLogs() {
+    setLoading(true);
     axios.get(route('activity-logs.model-activities', { model_type: modelType, model_id: modelId }))
       .then(response => {
         setLogs(response.data);
       })
       .catch(error => {
         console.error("Error fetching activity logs:", error);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }
 
   useEffect(() => {
     fetchActivityLogs();
   }, [modelType, modelId]);
+
+  if (loading) {
+    return (<LoadingSpinner size="large" />);
+  }
+
+  if (logs.length === 0) {
+    return (
+      <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="No activity logs found" />
+    );
+  }
 
   return (
     <div className="flex flex-col gap-2">
