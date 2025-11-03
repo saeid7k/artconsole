7641d24365bdebcdb6e8ab978713384b22c528b6
Saeid Khajevand <saeid.kh7@gmail.com>
2025-11-02 20:10:32 -0500

    Add relationships constant for managing user roles

---
+++ resources/js/constants/relationships.ts
@@ -0,0 +1,8 @@
+const RELATIONSHIPS = [
+  { value: 'artist', label: 'Artist' },
+  { value: 'vendor', label: 'Vendor' },
+  { value: 'collector', label: 'Collector' },
+  { value: 'other', label: 'Other' },
+]
+
+export default RELATIONSHIPS;
