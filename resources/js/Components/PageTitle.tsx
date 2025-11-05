22a1ce9362d475c57288ee8ccca4f0cd1762cbdd
Saeid Khajevand <saeid.kh7@gmail.com>
2025-11-05 11:57:55 -0500

    Add create button to PageTitle component with tooltip

--- resources/js/Components/PageTitle.tsx
+++ resources/js/Components/PageTitle.tsx
@@ -1,4 +1,6 @@
-import { Breadcrumb } from "antd";
+import { AddSquareIcon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { Breadcrumb, Button, Tooltip } from "antd";
 
 type Props = {
   title?: string | null;
@@ -7,9 +9,10 @@ type Props = {
   breadcrumbItems?: {
     title: string | React.ReactNode;
   }[];
+  onCreateButtonClick?: () => void;
 }
 
-function PageTitle({ title = null, counter = null, toolbar = null, breadcrumbItems = [] }: Props) {
+function PageTitle({ title = null, counter = null, toolbar = null, breadcrumbItems = [], onCreateButtonClick = undefined }: Props) {
   return (
       <div
         className="flex items-center justify-between flex-wrap gap-2 px-6 pb-3 w-full grow-0"
@@ -24,6 +27,17 @@ function PageTitle({ title = null, counter = null, toolbar = null, breadcrumbIte
           <div className="flex items-center gap-2">
             {title && <h3 className="m-0">{title}</h3>}
             {counter && <small className="text-muted font-light">({counter.toLocaleString()})</small>}
+            {onCreateButtonClick && (
+              <Tooltip title="Create New" mouseEnterDelay={1} >
+                <Button
+                  type="text"
+                  shape="circle"
+                  onClick={onCreateButtonClick}
+                >
+                  <HugeiconsIcon icon={AddSquareIcon} size={20} />
+                </Button>
+              </Tooltip>
+            )}
           </div>
         </div>
         {toolbar && <div>{toolbar}</div>}
