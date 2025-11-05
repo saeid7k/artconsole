297637ea2a86f64ee8e73b2338bbeff546d05165
Saeid Khajevand <saeid.kh7@gmail.com>
2025-11-04 23:46:23 -0500

    Add dayjs plugins for Ant Design DatePicker compatibility

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,4 +1,12 @@
 import axios from 'axios';
+import dayjs from 'dayjs';
+import localeData from 'dayjs/plugin/localeData';
+import weekday from 'dayjs/plugin/weekday';
+
+// Extend dayjs with plugins Ant Design DatePicker expects (weekday(), localeData(), etc.)
+dayjs.extend(weekday);
+dayjs.extend(localeData);
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
