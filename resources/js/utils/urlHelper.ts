7351af1eb6e0be60f2e2066c7285949c1dcbcd7a
Saeid Khajevand <saeid.kh7@gmail.com>
2025-11-05 21:51:36 -0500

    Add utility functions for query parameter management

---
+++ resources/js/utils/urlHelper.ts
@@ -0,0 +1,15 @@
+function getQueryParam(param: string): string | null {
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+  return params.get(param);
+}
+
+function deleteQueryParam(param: string): void {
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+  params.delete(param);
+  const newUrl = `${url.pathname}${params.toString().length > 0 ? '?' + params.toString() : ''}`;
+  window.history.replaceState({}, '', newUrl);
+}
+
+export { getQueryParam, deleteQueryParam }
