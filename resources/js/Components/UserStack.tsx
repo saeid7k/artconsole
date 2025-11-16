ea5af3806604e4cd2c5706315298f3902bf2f014
Saeid Khajevand <saeid.kh7@gmail.com>
2025-11-16 15:20:47 -0500

    fix shrink avatar in userStack

--- resources/js/Components/UserStack.tsx
+++ resources/js/Components/UserStack.tsx
@@ -9,7 +9,7 @@ type Props = {
 function UserStack({ user }: Props): JSX.Element {
   return (
     <div className='flex items-center gap-1'>
-      <Avatar size={'small'} className='text-xs' src={user.photo}>{getInitials(user.full_name)}</Avatar>
+      <Avatar size={'small'} className='text-xs shrink-0' src={user.photo}>{getInitials(user.full_name)}</Avatar>
       <div>{user.full_name}</div>
     </div>
   )
