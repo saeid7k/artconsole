cee98345af63b583160e0cb6a88703dd376e85cd
Saeid Khajevand <saeid.kh7@gmail.com>
2025-11-21 23:45:18 -0500

    fetch and display notifications in dropdown menu

--- resources/js/Components/NotificationsDropdown.tsx
+++ resources/js/Components/NotificationsDropdown.tsx
@@ -1,16 +1,44 @@
+import { NotificationProps } from "@/types/notification";
+import { keyToTitle } from "@/utils/stringHelper";
 import { Notification01Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { Button, Card, Dropdown, Menu } from "antd";
+import { Badge, Button, Dropdown, Menu } from "antd";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function NotificationsDropdown() {
+
+  const [notifications, setNotifications] = useState<NotificationProps[]>([]);
+
+  function fetchNotifications() {
+    axios.get(route('notifications.latest', { count: 5 }))
+      .then(response => {
+        setNotifications(response.data);
+      })
+      .catch(error => {
+        console.error('Failed to fetch notifications:', error);
+      });
+  }
+
+  useEffect(() => {
+    fetchNotifications();
+  }, []);
 
   const popup = () => {
     return (
       <Menu className="min-w-[200px]">
         <div className="text-center p-1">Notifications</div>
         <Menu.Divider />
-        {[1,2,3,4,5].map((n) => (
-          <Menu.Item key={n}>notification #{n}</Menu.Item>
+        {notifications.map((n) => (
+          <Menu.Item
+            key={n?.id}
+          >
+            <div className="flex items-center gap-1">
+              <Badge color="blue" />
+              <div>{keyToTitle(n?.type)}</div>
+            </div>
+            <div>{n?.data?.message}</div>
+          </Menu.Item>
         ))}
       </Menu>
     );
