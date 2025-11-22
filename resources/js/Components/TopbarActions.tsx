1961d0e75fd17bfb46d9dc9a0c4747287b4dc537
Saeid Khajevand <saeid.kh7@gmail.com>
2025-11-22 17:00:28 -0500

    adjust gap spacing in TopbarActions component

--- resources/js/Components/TopbarActions.tsx
+++ resources/js/Components/TopbarActions.tsx
@@ -4,7 +4,7 @@ import UserMenu from "./UserMenu";
 
 function TopbarActions() {
   return (
-    <div className="flex items-center gap-3">
+    <div className="flex items-center gap-4">
       <div className="flex items-center gap-2">
         <QuickCreateDropdown />
         <NotificationsDropdown />
