369b97e91c7ead9bc923728f401103b975be4f5b
Saeid Khajevand <saeid.kh7@gmail.com>
2025-11-26 00:39:45 -0500

    add invitedTo method to User model and include invitations in intervalData response

--- app/Http/Controllers/AppLayoutController.php
+++ app/Http/Controllers/AppLayoutController.php
@@ -12,9 +12,12 @@ class AppLayoutController extends Controller
 
     $notifications = $user->notifications()->latest()->take(5)->get();
 
+    $invitations = $user->invitedTo()->active()->with('gallery')->get();
+
     $data = [
       'unread_notifications_count' => $user->unreadNotifications()->count(),
       'latest_notifications' => $notifications,
+      'invitations' => $invitations,
     ];
 
     return response()->json($data);
