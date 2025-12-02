d1faf14339cf5a67aa3801dda3766e2ec1f89c11
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-02 00:41:46 -0500

    create migrations for locations and artworks tables

---
+++ database/migrations/2025_11_30_213310_create_locations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('locations', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('gallery_id')->constrained()->cascadeOnDelete();
+      $table->string('type', 100); // Internal | External | Venue | Client
+      $table->foreignId('contact_id')->nullOnDelete();
+      $table->string('name');
+      $table->text('description')->nullable();
+      $table->json('address')->nullable();
+      $table->boolean('is_primary')->default(false);
+      $table->boolean('is_active')->default(true);
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('locations');
+  }
+};
