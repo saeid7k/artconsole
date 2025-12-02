140ec64f8ba8958db8ecf9582684e23676589541
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-02 00:41:46 -0500

    create migrations for locations and artworks tables

---
+++ database/migrations/2025_11_30_213400_create_artworks_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Models\Contact;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('artworks', function (Blueprint $table) {
+      $table->id();
+      $table->foreignIdFor(User::class, 'creator_id')->nullOnDelete();
+      $table->foreignId('gallery_id')->constrained()->cascadeOnDelete();
+      $table->foreignId('location_id')->nullOnDelete();
+      $table->foreignIdFor(Contact::class, 'artist_id')->nullable()->nullOnDelete();
+
+      $table->string('sku', 10)->unique(); // PNT-25-001
+      $table->string('category', 100); // e.g., Painting, Sculpture, Photography
+      $table->json('edition', 100)->nullable(); // e.g., {type: (Unique | Limited Edition | Open Edition), number: 1, size: 50}
+
+      $table->string('title');
+      $table->string('subject', 100)->nullable();
+      $table->text('description')->nullable();
+      $table->string('year', 100)->nullable(); // e.g., 2023, Circa 19th Century
+      $table->json('dimensions')->nullable();
+      $table->decimal('price', 10, 2)->nullable();
+
+      $table->string('medium', 100)->nullable(); // e.g., Oil on Canvas
+      $table->json('styles')->nullable(); // e.g., Abstract, Realism
+      $table->json('collections')->nullable(); // e.g., Modern Art, Renaissance
+
+      $table->json('details')->nullable(); // Additional metadata
+      $table->text('notes')->nullable();
+
+      $table->string('status', 50)->default('available'); // e.g., available, sold, on loan
+
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('artworks');
+  }
+};
