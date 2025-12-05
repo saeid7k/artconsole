bdaad6038dffc3bfdb0f58b2b41e0f66a930baaf
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-05 18:48:17 -0500

    add ArtworkObserver to handle SKU generation on artwork creation

---
+++ app/Observers/ArtworkObserver.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Artwork;
+
+class ArtworkObserver
+{
+  /**
+   * Handle the User "created" event.
+   */
+  public function created(Artwork $artwork): void
+  {
+    // set SKU if not set
+    if (!$artwork->sku) {
+      $artwork->sku = Artwork::newSku($artwork->category);
+      $artwork->saveQuietly();
+    }
+  }
+
+  public function updated(Artwork $artwork): void
+  {
+    //
+  }
+
+  public function deleted(Artwork $artwork): void
+  {
+    //
+  }
+
+  public function restored(Artwork $artwork): void
+  {
+    //
+  }
+
+  public function forceDeleted(Artwork $artwork): void
+  {
+    //
+  }
+}
