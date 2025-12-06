f72da71b931ed6d03b1879775c3bf0308b111278
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-05 19:40:57 -0500

    add ArtworkController and index route for displaying artworks

--- app/Http/Controllers/ArtworkController.php
+++ app/Http/Controllers/ArtworkController.php
@@ -4,7 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ArtworksController extends Controller
+class ArtworkController extends Controller
 {
-    //
+  public function index(Request $request)
+  {
+    $user = auth()->user();
+    $gallery = $user->currentGallery();
+    $artworks = $gallery->artworks()
+      ->with([
+        'artist' => function ($q) {
+          $q->select('id', 'firstname', 'lastname');
+        },
+      ])
+      ->paginate($request->per_page ?? 10)->withQueryString();
+
+    return inertia('Artworks/Index', [
+      'artworks' => $artworks,
+    ]);
+  }
 }
