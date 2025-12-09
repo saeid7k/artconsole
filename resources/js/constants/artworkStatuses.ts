6c5718e15d5079c0091028422b4ce7819a7de38d
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-09 00:58:45 -0500

    add ArtworkStatus enum and ARTWORK_STATUSES constant for artwork status management

---
+++ resources/js/constants/artworkStatuses.ts
@@ -0,0 +1,9 @@
+const ARTWORK_STATUSES = [
+  { label: 'Available', value: 'available', color: 'green' },
+  { label: 'Sold', value: 'sold', color: 'red' },
+  { label: 'On Loan', value: 'on_loan', color: 'blue' },
+  { label: 'In Transit', value: 'in_transit', color: 'orange' },
+  { label: 'In Exhibition', value: 'in_exhibition', color: 'purple' },
+];
+
+export default ARTWORK_STATUSES;
