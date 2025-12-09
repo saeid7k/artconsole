9eab7e03fbff639500c87b50e73a551953f74041
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-08 22:49:34 -0500

    add accent color shades to CSS and update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -59,8 +59,18 @@ export default {
     				foreground: 'hsl(var(--muted-foreground))'
     			},
     			accent: {
-    				DEFAULT: 'hsl(var(--accent))',
-    				foreground: 'hsl(var(--accent-foreground))'
+    				DEFAULT: 'var(--accent)',
+    				foreground: 'var(--accent-foreground)',
+            50: 'var(--accent-50)',
+            100: 'var(--accent-100)',
+            200: 'var(--accent-200)',
+            300: 'var(--accent-300)',
+            400: 'var(--accent-400)',
+            500: 'var(--accent-500)',
+            600: 'var(--accent-600)',
+            700: 'var(--accent-700)',
+            800: 'var(--accent-800)',
+            900: 'var(--accent-900)',
     			},
     			destructive: {
     				DEFAULT: 'hsl(var(--destructive))',
