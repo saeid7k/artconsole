aff8344554ee28adc7a438c2090e20d9e5e9839c
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-09 11:51:56 -0500

    add LocationProps interface to define location structure

---
+++ resources/js/types/location.d.ts
@@ -0,0 +1,15 @@
+import { Address } from "./commonObjects";
+
+export interface LocationProps {
+  id: number;
+  type: string;
+  contact_id?: number|null;
+  name: string;
+  description?: string|null;
+  address?: Address|null;
+  address_same_as_gallery: boolean;
+  is_primary: boolean;
+  is_active: boolean;
+  created_at: string;
+  updated_at: string;
+}
