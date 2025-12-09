594f2f7761e7d2c43022e24ca1763b70ef90f5a1
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-09 00:58:45 -0500

    add ArtworkStatus enum and ARTWORK_STATUSES constant for artwork status management

--- app/Enums/ArtworkStatus.php
+++ app/Enums/ArtworkStatus.php
@@ -9,6 +9,8 @@ enum ArtworkStatus: string
   case Available = 'available';
   case Sold = 'sold';
   case OnLoan = 'on_loan';
+  case InTransit = 'in_transit';
+  case InExhibition = 'in_exhibition';
 
   public function label(): string
   {
@@ -20,7 +22,9 @@ enum ArtworkStatus: string
     return match ($this) {
       self::Available => 'green',
       self::Sold => 'red',
-      self::OnLoan => 'blue'
+      self::OnLoan => 'blue',
+      self::InTransit => 'orange',
+      self::InExhibition => 'purple',
     };
   }
 }
