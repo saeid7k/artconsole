40c3a4fe2188801520f90d82faf02faa94e710a2
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-09 19:15:30 -0500

    add view mode toggle for artworks display; implement grid view and update search toolbar

--- resources/js/Pages/Artworks/Index.tsx
+++ resources/js/Pages/Artworks/Index.tsx
@@ -1,14 +1,21 @@
 import PageTitle from "@/Components/PageTitle"
+import { useSearch } from "@/hooks/useSearch"
 import AppLayout from "@/Layouts/AppLayout"
 import { PageProps } from "@/types"
-import Search from "antd/es/input/Search"
-import ArtworksTable from "./Partials/ArtworksTable"
-import { useSearch } from "@/hooks/useSearch"
 import { LocationProps } from "@/types/location"
+import { GridViewIcon, TableIcon } from "@hugeicons/core-free-icons"
+import { HugeiconsIcon } from "@hugeicons/react"
+import { Segmented, Tooltip } from "antd"
+import Search from "antd/es/input/Search"
+import { useState } from "react"
+import ArtworksGrids from "./Partials/ArtworksGrids"
+import ArtworksTable from "./Partials/ArtworksTable"
 
 function Index({ artworks, locations }: { artworks: PageProps, locations: Array<LocationProps> }) {
 
   const { handleSearch } = useSearch('artworks.index');
+
+  const [viewMode, setViewMode] = useState<'table' | 'grid'>('table');
 
   return (
     <div>
@@ -17,15 +24,43 @@ function Index({ artworks, locations }: { artworks: PageProps, locations: Array<
         counter={artworks.total}
         // onCreateButtonClick={() => {}}
         toolbar={
-          <Search
-            placeholder="search artworks..."
-            style={{ width: 200 }}
-            allowClear
-            onSearch={handleSearch}
-          />
+          <div className="flex gap-2">
+            <Segmented
+              options={[
+                {
+                  label: (
+                    <Tooltip title="Table View" mouseEnterDelay={1}>
+                      <div className="flex h-[28px] items-center"><HugeiconsIcon icon={TableIcon} size={20} /></div>
+                    </Tooltip>
+                  ),
+                  value: 'table'
+                },
+                {
+                  label: (
+                    <Tooltip title="Grid View" mouseEnterDelay={1}>
+                      <div className="flex h-[28px] items-center"><HugeiconsIcon icon={GridViewIcon} size={20} /></div>
+                    </Tooltip>
+                  ),
+                  value: 'grid'
+                },
+              ]}
+              onChange={(value) => setViewMode(value as 'table' | 'grid')}
+            />
+            <Search
+              placeholder="search artworks..."
+              style={{ width: 200 }}
+              size="middle"
+              allowClear
+              onSearch={handleSearch}
+            />
+          </div>
         }
       />
-      <ArtworksTable artworks={artworks} locations={locations} />
+      {viewMode === 'table' ? (
+        <ArtworksTable artworks={artworks} locations={locations} />
+      ) : (
+        <ArtworksGrids />
+      )}
     </div>
   )
 }
---
+++ resources/js/Pages/Artworks/Partials/ArtworksGrids.tsx
@@ -0,0 +1,8 @@
+import { PageProps } from "@/types";
+
+function ArtworksGrids({ artworks }: { artworks?: PageProps }) {
+  return (
+    <div>coming soon</div>
+  )
+}
+export default ArtworksGrids;
