691955f2feacb03449070baf50cdb08626cd8ccb
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-09 23:54:49 -0500

    add rootClassName prop to ArtworkTitleStack component

--- resources/js/Components/ArtworkTitleStack.tsx
+++ resources/js/Components/ArtworkTitleStack.tsx
@@ -4,14 +4,15 @@ import { Tooltip } from "antd";
 
 type Props = {
   artwork: ArtworkProps
+  rootClassName?: string;
 }
 
-function ArtworkTitleStack({ artwork }: Props) {
+function ArtworkTitleStack({ artwork, rootClassName }: Props) {
 
   const artistName = artwork.artist_data.full_name;
 
   return (
-    <div className="flex flex-col">
+    <div className={`flex flex-col ${rootClassName || ''}`}>
       <div className="font-semibold">{artwork.title}</div>
       <div>
         <span className="text-muted italic">by </span>
