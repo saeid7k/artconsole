5fb39e1fbeaccae0897a88315293e8f8171ba40b
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-13 12:33:15 -0500

    fix file formatting of ContactController

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -80,17 +80,17 @@ class ContactController extends Controller
     //
   }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Contact $contact)
-    {
-      $this->authorize('view', $contact);
+  /**
+   * Display the specified resource.
+   */
+  public function show(Contact $contact)
+  {
+    $this->authorize('view', $contact);
 
-      return inertia('Contacts/View', [
-        'contact' => $contact
-      ]);
-    }
+    return inertia('Contacts/View', [
+      'contact' => $contact
+    ]);
+  }
 
   /**
    * Show the form for editing the specified resource.
