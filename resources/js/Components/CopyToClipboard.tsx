caf4db48923eb9f4ba1be5316ff28b159693c23b
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-13 12:30:31 -0500

    create CopyToClipboard component for copying text to clipboard

---
+++ resources/js/Components/CopyToClipboard.tsx
@@ -0,0 +1,42 @@
+import { Copy01Icon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { message, Tooltip } from "antd";
+
+type Props = {
+  content: string | number | null;
+  title?: string | null;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+}
+
+function CopyToClipboard({ content, title = null, size = 'md' }: Props) {
+
+  function copyText(text: any) {
+    let titleToUse = title || 'Text';
+    navigator.clipboard.writeText(text).then(() => {
+      message.success(titleToUse + ' copied to clipboard');
+    }).catch((err) => {
+      message.error('Failed to copy ' + titleToUse + ': ' + err);
+    });
+  }
+
+  const iconSizeMap = {
+    'xs': 12,
+    'sm': 14,
+    'md': 16,
+    'lg': 20,
+    'xl': 24,
+  }
+
+  return (
+    <Tooltip title={`Copy to Clipboard`} mouseEnterDelay={1} placement="right" >
+      <HugeiconsIcon
+        icon={Copy01Icon}
+        size={iconSizeMap[size]}
+        className="cursor-pointer text-muted hover:text-primary"
+        onClick={() => copyText(content)}
+      />
+    </Tooltip>
+  )
+}
+
+export default CopyToClipboard;
