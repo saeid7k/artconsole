747cfa9531e588f6f8de7abaa6672bceb58ac72a
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-13 22:45:59 -0500

    add ARTWORK_EDITIONS constant for artwork edition types

---
+++ resources/js/constants/artworkEditions.ts
@@ -0,0 +1,7 @@
+const ARTWORK_EDITIONS = [
+  { label: 'Unique Piece', value: 'unique' },
+  { label: 'Limited Edition', value: 'limited' },
+  { label: 'Open Edition', value: 'open' },
+]
+
+export default ARTWORK_EDITIONS;
