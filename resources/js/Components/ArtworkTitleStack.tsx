430c89a530285462ed47c2900652f804313f3f9d
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-13 22:46:48 -0500

    add showEdition prop to ArtworkTitleStack and update ArtworkCard to pass it

--- resources/js/Components/ArtworkTitleStack.tsx
+++ resources/js/Components/ArtworkTitleStack.tsx
@@ -1,25 +1,33 @@
 import { ArtworkProps } from "@/types/artwork";
 import { Link } from "@inertiajs/react";
 import { Tooltip } from "antd";
+import FormattedEdition from "./FormattedEdition";
 
 type Props = {
   artwork: ArtworkProps
   rootClassName?: string;
   artistTooltipPlacement?: "top" | "left" | "right" | "bottom" | "topLeft" | "topRight" | "bottomLeft" | "bottomRight" | "leftTop" | "leftBottom" | "rightTop" | "rightBottom";
+  showEdition?: boolean;
 }
 
-function ArtworkTitleStack({ artwork, rootClassName, artistTooltipPlacement = "bottom" }: Props) {
+function ArtworkTitleStack({ artwork, rootClassName, artistTooltipPlacement = "bottom", showEdition = true }: Props) {
 
   const artistName = artwork.artist_data.full_name;
 
   return (
     <div className={`flex flex-col ${rootClassName || ''}`}>
+
+      {/* Title */}
+
       <Link
         className="text-body font-semibold"
         href={route('artworks.show', artwork.id)}
       >
         {artwork.title}
       </Link>
+
+      {/* Artist */}
+
       <div>
         <span className="text-muted italic">by </span>
         {artwork.artist ? (
@@ -36,6 +44,14 @@ function ArtworkTitleStack({ artwork, rootClassName, artistTooltipPlacement = "b
           <span className="text-accent">{artistName}</span>
         )}
       </div>
+
+      {/* Edition */}
+
+      {showEdition && (
+        <div>
+          <FormattedEdition edition={artwork.edition} />
+        </div>
+      )}
     </div>
   );
 }
