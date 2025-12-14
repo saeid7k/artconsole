e973415d0975b659145415c22cdca5d7be577b14
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-14 00:54:39 -0500

    make title prop optional in DataCol component

--- resources/js/Components/Containers/DataCol.tsx
+++ resources/js/Components/Containers/DataCol.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from 'react';
 
-function DataCol({ title, children }: { title: string; children: ReactNode }) {
+function DataCol({ title, children }: { title?: string; children: ReactNode }) {
   return (
     <div className="flex flex-col gap-1">
-      <h4 className="m-0">{title}</h4>
+      {title && <h4 className="m-0">{title}</h4>}
       {children}
     </div>
   )
