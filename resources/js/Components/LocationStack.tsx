2c44267d52ffcdd945b6de66ae44835febe0fbac
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-24 00:00:59 -0500

    refactor LocationStack component to use LocationProps for better structure and clarity

--- resources/js/Components/LocationStack.tsx
+++ resources/js/Components/LocationStack.tsx
@@ -1,29 +1,29 @@
 import colors from "@/Themes/theme";
+import { LocationProps } from "@/types/location";
 import { StoreLocation01Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { twMerge } from "tailwind-merge";
 
 type Props = {
-  name: string;
-  address: string;
+  location: LocationProps;
   showAddress?: boolean;
   clamped?: boolean;
 }
 
-function LocationStack({ name, address, showAddress = false, clamped = true }: Props) {
+function LocationStack({ location, showAddress = false, clamped = true }: Props) {
   return(
-    <div className="flex items-center gap-1">
-      <HugeiconsIcon icon={StoreLocation01Icon} color={colors.gray[400]} />
+    <div className="flex items-start gap-1">
+      <HugeiconsIcon icon={StoreLocation01Icon} color={colors.gray[400]} className="pt-0.5" />
       <div className="flex flex-col">
-        <div>{name}</div>
+        <div>{location.name}</div>
         { showAddress && (
           <div
             className={twMerge(
               'text-muted',
               clamped ? 'max-w-[200px] line-clamp-1' : ''
             )}
-            title={clamped ? address : undefined}
-          >{address}</div>
+            title={clamped ? location.formatted_address : undefined}
+          >{location.formatted_address}</div>
         )}
       </div>
     </div>
