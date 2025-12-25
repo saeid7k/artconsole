fd2ae09107f89cc2ea80395f5b9d48bde76c2b33
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-24 22:51:29 -0500

    update artwork statuses options

--- resources/js/constants/artworkStatuses.ts
+++ resources/js/constants/artworkStatuses.ts
@@ -2,9 +2,8 @@ const ARTWORK_STATUSES = [
   { label: 'Available', value: 'available', color: 'green' },
   { label: 'On Hold', value: 'on_hold', color: 'orange' },
   { label: 'Sold', value: 'sold', color: 'red' },
-  { label: 'Consigned Out', value: 'consigned_out', color: 'teal' },
-  { label: 'On Loan', value: 'on_loan', color: 'blue' },
   { label: 'In Transit', value: 'in_transit', color: 'purple' },
+  { label: 'Consigned Out', value: 'consigned_out', color: 'teal' },
 ];
 
 export default ARTWORK_STATUSES;
