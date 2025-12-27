42efcdf314902d1a390925c60e38fa5f0e5d31c3
Saeid Khajevand <saeid.kh7@gmail.com>
2025-12-26 19:07:30 -0500

    create ContactAvatar comp. and apply in ContactStack

--- resources/js/Components/ContactStack.tsx
+++ resources/js/Components/ContactStack.tsx
@@ -1,10 +1,9 @@
 import { ContactProps } from "@/types/contact";
-import { stringToColor } from "@/utils/colorHelper";
-import { getInitials } from "@/utils/stringHelper";
-import { EyeIcon, ViewIcon } from "@hugeicons/core-free-icons";
+import { ViewIcon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { router } from "@inertiajs/react";
-import { Avatar, Tooltip } from "antd";
+import { Tooltip } from "antd";
+import ContactAvatar from "./ContactAvatar";
 
 function ContactStack({contact}: {contact: ContactProps}) {
 
@@ -13,14 +12,7 @@ function ContactStack({contact}: {contact: ContactProps}) {
       className="flex items-center gap-2 cursor-pointer group"
       onClick={() => router.get(route('contacts.show', contact.id))}
     >
-      <Avatar
-        shape="square"
-        className="tracking-wider min-w-8 border-none"
-        style={{ backgroundColor: stringToColor(getInitials(contact.full_name)) }}
-        src={contact.photo}
-      >
-        {getInitials(contact.full_name)}
-      </Avatar>
+      <ContactAvatar contact={contact} />
       <div>
         <div className="leading-tight">{contact.full_name}</div>
         <div className="text-ghost leading-tight">{contact.business?.name}</div>
