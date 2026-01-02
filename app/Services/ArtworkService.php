e7990588947fbadeb5171d7587e63d281bd1d23f
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-01 23:46:26 -0500

    move newSku method to ArtworkService

---
+++ app/Services/ArtworkService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Enums\ArtworkCategory;
+use App\Models\Artwork;
+
+class ArtworkService
+{
+  protected $user;
+  protected $gallery;
+  protected $artwork;
+
+  public function __construct(?Artwork $artwork = null)
+  {
+    $this->user = auth()->user();
+    $this->artwork = $artwork;
+    $this->gallery = $artwork ? $artwork->gallery : $this->user->currentGallery;
+  }
+
+  public function newSku(?string $category): string
+  {
+    $category = $category ?? ($this->artwork ? $this->artwork->category : ArtworkCategory::default()->value);
+
+    $prefix = ArtworkCategory::from($category)->code();
+    $year = date('y');
+
+    $latestArtwork = $this->gallery->artworks()->where('sku', 'like', "{$prefix}-{$year}-%")
+      ->orderBy('sku', 'desc')
+      ->first();
+
+    if ($latestArtwork) {
+      $parts = explode('-', $latestArtwork->sku);
+      $number = (int) $parts[2] + 1;
+    } else {
+      $number = 1;
+    }
+
+    return sprintf("%s-%s-%03d", $prefix, $year, $number);
+  }
+}
