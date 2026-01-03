563411b49d81f571cf4d8fb561c8b475a6d4bf9b
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-03 00:47:25 -0500

    rename ContactWidget component for displaying contact information with view and unset options

--- resources/js/Components/ContactWidget.tsx
+++ resources/js/Components/ContactWidget.tsx
@@ -5,37 +5,38 @@ import { Button, Tooltip } from 'antd';
 import ContactAvatar from './ContactAvatar';
 
 type Props = {
-  artist: ContactProps;
+  contact: ContactProps;
+  title?: string;
   viewButton?: boolean;
   unsetFunction?: () => void;
   className?: string;
 }
 
-function ArtistStack({ artist, viewButton = true, unsetFunction, className }: Props) {
+function ContactWidget({ contact, title = 'Contact', viewButton = true, unsetFunction, className }: Props) {
   return (
     <div className={`flex gap-1 ${className}`}>
-      <div className="flex items-center gap-1 border border-dashed border-soft rounded py-1 px-2 w-max">
-        <ContactAvatar contact={artist} />
+      <div className="flex items-center gap-1 border border-dashed border-soft rounded py-1 px-2 w-max h-max">
+        <ContactAvatar contact={contact} />
         <div className="flex flex-col leading-tight">
-          <label className=''>Artist</label>
-          <div>{artist?.full_name}</div>
+          <label>{title}</label>
+          <div>{contact?.full_name}</div>
         </div>
       </div>
       <div className="flex flex-col">
         {viewButton && (
-          <Tooltip title="View Artist" placement="right">
+          <Tooltip title="View in new tab" placement="right">
             <Button
               size='small'
               variant='text'
               color='blue'
               icon={<HugeiconsIcon icon={LinkSquare02Icon} size={16} />}
-              href={artist ? route('contacts.show', artist.id) : '#'}
+              href={contact ? route('contacts.show', contact.id) : '#'}
               target="_blank"
             />
           </Tooltip>
         )}
         {typeof unsetFunction === 'function' && (
-          <Tooltip title="Unset Artist" placement="right">
+          <Tooltip title="Unset" placement="right">
             <Button
               size='small'
               variant='text'
@@ -50,4 +51,4 @@ function ArtistStack({ artist, viewButton = true, unsetFunction, className }: Pr
   )
 }
 
-export default ArtistStack;
+export default ContactWidget;
