4c2ac3bee68eac0afd5b1b954834d7d31fab917e
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-04 21:21:19 -0500

    update LocationStack to integrate MoveModal

--- resources/js/Components/LocationStack.tsx
+++ resources/js/Components/LocationStack.tsx
@@ -1,9 +1,11 @@
 import colors from "@/Themes/theme";
 import { LocationProps } from "@/types/location";
-import { ArrowDataTransferDiagonalIcon, StoreLocation01Icon } from "@hugeicons/core-free-icons";
+import { ArrowDataTransferHorizontalIcon, StoreLocation01Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { Button, Tooltip } from "antd";
+import { useState } from "react";
 import { twMerge } from "tailwind-merge";
+import MoveModal from "./MoveModal";
 
 type Props = {
   location: LocationProps;
@@ -16,43 +18,55 @@ type Props = {
 }
 
 function LocationStack({ location, showActions = true, showAddress = false, clamped = true, boxed = false, bordered = false, className }: Props) {
+
+  const [openMoveModal, setOpenMoveModal] = useState(false);
+
   return(
-    <div className="flex gap-1">
-      <div
-        className={twMerge(
-          "flex items-start gap-1",
-          boxed ? 'p-2 bg-light rounded' : '',
-          bordered ? 'border border-solid border-light' : '',
-          className
-        )}
-      >
-        <HugeiconsIcon icon={StoreLocation01Icon} color={colors.gray[400]} className="pt-0.5" />
-        <div className="flex flex-col">
-          <div>{location.name}</div>
-          { showAddress && (
-            <div
-              className={twMerge(
-                'text-muted',
-                clamped ? 'max-w-[200px] line-clamp-1' : ''
-              )}
-              title={clamped ? location.formatted_address : undefined}
-            >{location.formatted_address}</div>
+    <>
+      <div className="flex gap-1">
+        <div
+          className={twMerge(
+            "flex items-start gap-1",
+            boxed ? 'p-2 bg-light rounded' : '',
+            bordered ? 'border border-solid border-light' : '',
+            className
           )}
+        >
+          <HugeiconsIcon icon={StoreLocation01Icon} color={colors.gray[400]} className="pt-0.5" />
+          <div className="flex flex-col">
+            <div>{location.name}</div>
+            { showAddress && (
+              <div
+                className={twMerge(
+                  'text-muted',
+                  clamped ? 'max-w-[200px] line-clamp-1' : ''
+                )}
+                title={clamped ? location.formatted_address : undefined}
+              >{location.formatted_address}</div>
+            )}
+          </div>
         </div>
+        {showActions && (
+          <div className="flex flex-col">
+            <Tooltip title="Move" placement="right">
+              <Button
+                size="small"
+                variant="text"
+                icon={<HugeiconsIcon icon={ArrowDataTransferHorizontalIcon} size={16} />}
+                color="blue"
+                onClick={() => setOpenMoveModal(true)}
+              />
+            </Tooltip>
+          </div>
+        )}
       </div>
-      { showActions && (
-        <div className="flex flex-col">
-          <Tooltip title="Move" placement="right">
-            <Button
-              size="small"
-              variant="text"
-              icon={<HugeiconsIcon icon={ArrowDataTransferDiagonalIcon} size={16} />}
-              color="blue"
-            />
-          </Tooltip>
-        </div>
+      {showActions && (
+        <MoveModal
+          open={openMoveModal}
+          setOpen={setOpenMoveModal}
+        />
       )}
-    </div>
+    </>
   )
 }
 
