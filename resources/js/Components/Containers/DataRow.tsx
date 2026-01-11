4c14cbab71c2fb4d578f0fb7cd8d2312fa9bdb90
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-11 00:09:35 -0500

    extend DataRow components

--- resources/js/Components/Containers/DataRow.tsx
+++ resources/js/Components/Containers/DataRow.tsx
@@ -1,17 +1,28 @@
 import { ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
 
 type Props = {
-  icon: ReactNode
-  label: string | ReactNode
+  icon?: ReactNode | null
+  label?: string | ReactNode | null
   value: string | ReactNode
+  wrapping?: boolean
+  align?: 'center' | 'start' | 'end'
   labelClassName?: string
+  rootClassName?: string
 }
 
-function DataRow({ icon, label, value, labelClassName = '' }: Props) {
+function DataRow({ icon = null, label = null, value, wrapping = true, align = 'center', labelClassName = '', rootClassName = '' }: Props) {
   return (
-    <div className="flex items-center gap-0 flex-wrap">
+    <div className={twMerge(
+        'flex items-center gap-0',
+        wrapping ? 'flex-wrap' : 'flex-nowrap',
+        `items-${align}`,
+        rootClassName
+      )}>
       {icon}
-      <label className={`ms-1 whitespace-nowrap ${labelClassName}`}>{label}</label>
+      {label && (
+        <label className={`ms-1 whitespace-nowrap ${labelClassName}`}>{label}</label>
+      )}
       <span className="ms-2">{value}</span>
     </div>
   )
