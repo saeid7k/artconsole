c7f683a8a5fddf16418765bdfd40b4c3c0ab55c1
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-11 22:21:21 -0500

    add showCopyToClipboard prop to DataRow component

--- resources/js/Components/Containers/DataRow.tsx
+++ resources/js/Components/Containers/DataRow.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react"
 import { twMerge } from "tailwind-merge"
+import CopyToClipboard from "../CopyToClipboard"
 
 type Props = {
   icon?: ReactNode | null
@@ -7,11 +8,24 @@ type Props = {
   value: string | ReactNode
   wrapping?: boolean
   align?: 'center' | 'start' | 'end'
+  showCopyToClipboard?: boolean
   labelClassName?: string
   rootClassName?: string
 }
 
-function DataRow({ icon = null, label = null, value, wrapping = true, align = 'center', labelClassName = '', rootClassName = '' }: Props) {
+function DataRow({
+  icon = null,
+  label = null,
+  value,
+  wrapping = true,
+  align = 'center',
+  showCopyToClipboard = false,
+  labelClassName = '',
+  rootClassName = ''
+}: Props) {
+
+  const isCopyToClipboardAvailable = showCopyToClipboard && typeof value === 'string' && value.length > 0
+
   return (
     <div className={twMerge(
         'flex items-center gap-0',
@@ -24,6 +38,14 @@ function DataRow({ icon = null, label = null, value, wrapping = true, align = 'c
         <label className={`ms-1 whitespace-nowrap ${labelClassName}`}>{label}</label>
       )}
       <span className="ms-2">{value}</span>
+      {isCopyToClipboardAvailable && (
+        <CopyToClipboard
+          title={typeof label === 'string' ? label : null}
+          content={value}
+          size="lg"
+          className="ms-1"
+        />
+      )}
     </div>
   )
 }
