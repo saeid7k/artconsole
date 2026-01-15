76ea532245b8a4babf193bcc45abc3f53585adc3
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-14 21:43:37 -0500

    create LOCATION_TYPES constant

---
+++ resources/js/constants/locationTypes.ts
@@ -0,0 +1,7 @@
+const LOCATION_TYPES = [
+  { value: 'internal', label: 'Internal', default: true },
+  { value: 'external', label: 'External' },
+  { value: 'venue', label: 'Venue' },
+]
+
+export { LOCATION_TYPES };
