a8929b63dcde0d4a82b082f7c373177bc14e0754
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-17 16:23:34 -0500

    add formatByKey and formatDimensions to formatHelper

--- resources/js/utils/formatHelper.ts
+++ resources/js/utils/formatHelper.ts
@@ -1,3 +1,35 @@
+import { DimensionsProps } from "@/types/dimensions";
+import { keyToTitle, stringifyObject } from "./stringHelper";
+import { formatAddress } from "./addressHelper";
+import dayjs from "dayjs";
+
+function formatByKey(key: string, value: any): any {
+  switch (key) {
+    case 'phone':
+    case 'mobile':
+      return formatPhoneNumber(value);
+    case 'website':
+    case 'url':
+      return trimWebsite(value);
+    case 'price':
+    case 'cost':
+    case 'amount':
+      return formatCurrency(value);
+    case 'dimensions':
+      return formatDimensions({ dimensions: value, showDepth: true });
+    case 'address':
+      return formatAddress(value);
+    case 'date':
+    case 'birthday':
+      return value ? dayjs(value).format('MMM D, YYYY') : '';
+    default:
+      return typeof value === 'object' ?
+        stringifyObject(value)
+        :
+        typeof value === 'string' ? keyToTitle(value) : value;
+  }
+}
+
 function formatPhoneNumber(phoneNumber: string): string {
   if (!phoneNumber) return '';
 
@@ -38,4 +70,19 @@ function formatCurrency(amount: number | string, maximumFractionDigits: number =
   }).format(num);
 }
 
-export { formatPhoneNumber, trimWebsite, formatCurrency };
+function formatDimensions({dimensions, showDepth = false}: {dimensions: DimensionsProps | null; showDepth?: boolean;}): string {
+  if (!dimensions) {
+    return '-';
+  }
+
+  const { width, height, depth, unit } = dimensions;
+  const unitSymbol = unit === 'inches' ? '"' : unit === 'cm' ? ' cm' : '';
+  let dimensionString = `${width}${unitSymbol} x ${height}${unitSymbol}`;
+  if (showDepth && depth) {
+    dimensionString += ` x ${depth}${unitSymbol}`;
+  }
+
+  return dimensionString;
+}
+
+export { formatByKey, formatPhoneNumber, trimWebsite, formatCurrency, formatDimensions };
