faceb744def186cb4ad0cf5551b5c60325e1e7c5
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-16 20:51:28 -0500

    add artwork creation functionality to QuickCreateDropdown and handle action query in Artworks Index

--- resources/js/Components/QuickCreateDropdown.tsx
+++ resources/js/Components/QuickCreateDropdown.tsx
@@ -1,13 +1,26 @@
-import { AddSquareIcon, ContactIcon } from "@hugeicons/core-free-icons";
+import { AddSquareIcon, ContactIcon, ImageAdd02Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { router } from "@inertiajs/react";
 import { Button, Dropdown, Menu } from "antd";
+import FlexBox from "./Containers/FlexBox";
 
 function QuickCreateDropdown() {
 
   const popup = () => {
     return (
       <Menu>
+        <Menu.Item key="new-artwork"
+          onClick={() => {router.visit(route('artworks.index'), {
+            data: {
+              action: 'create',
+            }
+          })}}
+        >
+          <FlexBox gap={2} >
+            <HugeiconsIcon icon={ImageAdd02Icon} size={20} />
+            New Artwork
+          </FlexBox>
+        </Menu.Item>
         <Menu.Item key="new-contact"
           onClick={() => {router.visit(route('contacts.index'), {
             data: {
@@ -15,10 +28,10 @@ function QuickCreateDropdown() {
             }
           })}}
         >
-          <div className="flex items-center gap-2">
+          <FlexBox gap={2} >
             <HugeiconsIcon icon={ContactIcon} size={20} />
             New Contact
-          </div>
+          </FlexBox>
         </Menu.Item>
       </Menu>
     );
