5585c4f5d2fb2c9669575456ab633b2d691de81a
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-17 16:23:51 -0500

    refactor FormattedDimensions to use formatDimensions utility

--- resources/js/Components/FormattedDimensions.tsx
+++ resources/js/Components/FormattedDimensions.tsx
@@ -1,25 +1,14 @@
+import { DimensionsProps } from "@/types/dimensions";
+import { formatDimensions } from "@/utils/formatHelper";
+
 type Props = {
-  dimensions: {
-    width: number;
-    height: number;
-    depth: number;
-    unit: string;
-  } | null;
+  dimensions: DimensionsProps | null;
   showDepth?: boolean;
   className?: string;
 }
 
 function FormattedDimensions({ dimensions, showDepth = false, className = "" }: Props) {
-  if (!dimensions) {
-    return <span>N/A</span>;
-  }
-
-  const { width, height, depth, unit } = dimensions;
-  const unitSymbol = unit === 'inches' ? '"' : unit === 'cm' ? ' cm' : '';
-  let dimensionString = `${width}${unitSymbol} x ${height}${unitSymbol}`;
-  if (showDepth && depth) {
-    dimensionString += ` x ${depth}${unitSymbol}`;
-  }
+  const dimensionString = formatDimensions({ dimensions, showDepth });
 
   return (
     <div
