169660bcd823a957e0160d3b41f5b9735ba8b1c8
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-17 13:44:22 -0500

    add props to ActivityLogProps interface

--- resources/js/types/activityLog.d.ts
+++ resources/js/types/activityLog.d.ts
@@ -2,9 +2,12 @@ import { UserProps } from "./user";
 
 export interface ActivityLogProps {
   id: number;
+  log_name: string;
+  event: string;
   causer?: UserProps;
   description: string;
   photo?: string;
+  properties: Record<string, any>;
   created_at: string;
   updated_at: string;
 }
