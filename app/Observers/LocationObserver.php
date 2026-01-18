1eff45aaf99e8214ccf8a582b141a07d2bceb2e3
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-18 18:39:13 -0500

    fill coordinates in Location Observer saved method

--- app/Observers/LocationObserver.php
+++ app/Observers/LocationObserver.php
@@ -2,6 +2,8 @@
 
 namespace App\Observers;
 
+use App\Helpers\AddressHelper;
+use App\Helpers\DataHelper;
 use App\Models\Location;
 
 class LocationObserver
@@ -9,12 +11,14 @@ class LocationObserver
   public function saved(Location $location)
   {
     // Clear address if same as gallery
+
     if ($location->address_same_as_gallery) {
       $location->address = null;
       $location->saveQuietly();
     }
 
     // Ensure only one primary location per gallery
+
     if ($location->is_primary) {
       Location::where('gallery_id', $location->gallery_id)
         ->where('id', '!=', $location->id)
@@ -36,6 +40,7 @@ class LocationObserver
     }
 
     // Change primary if current primary is deactivated
+
     if (!$location->is_active && $location->is_primary) {
       $anotherActiveLocation = Location::where('gallery_id', $location->gallery_id)
         ->where('is_active', true)
@@ -46,6 +51,23 @@ class LocationObserver
         $location->updateQuietly(['is_primary' => false]);
       } else {
         $location->updateQuietly(['is_active' => true]);
+      }
+    }
+
+    // Fill coordinates
+
+    $address = $location->address;
+    $coordinates = $address?->coordinates ?? null;
+    $coordinatesIsNotFilled = empty($coordinates?->lat) || empty($coordinates?->lng);
+
+    if (($address?->street && $coordinatesIsNotFilled) || $location->wasChanged('address')) {
+      $coordinates = AddressHelper::addressToCoordinates($location->formatted_address);
+      if ($coordinates) {
+        $address = DataHelper::objectToArray($location->address);
+        $address['coordinates']['lat'] = $coordinates['lat'];
+        $address['coordinates']['lng'] = $coordinates['lng'];
+        $location->address = DataHelper::arrayToObject($address);
+        $location->saveQuietly();
       }
     }
   }
