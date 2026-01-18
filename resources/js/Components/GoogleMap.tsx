a5a54996d7e9116744891c847a9e323b664569b3
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-18 18:44:20 -0500

    update GoogleMap component how to handle coordinates and add hideIfNotFound prop

--- resources/js/Components/GoogleMap.tsx
+++ resources/js/Components/GoogleMap.tsx
@@ -1,27 +1,46 @@
+import { twMerge } from "tailwind-merge"
+
 type Props = {
-  lat: number
-  lng: number
+  coordinates: {
+    lat: number | null
+    lng: number | null
+  } | undefined | null
   zoom?: number
   width?: string | number
   height?: string | number
   mapType?: 'roadmap' | 'satellite'
+  hideIfNotFound?: boolean
   className?: string
 } & React.IframeHTMLAttributes<HTMLIFrameElement>
 
-function GoogleMap({ lat, lng, zoom = 14, width = '100%', height = '400px', mapType = 'roadmap', className = '', ...props }: Props) {
+function GoogleMap({ coordinates, zoom = 14, width = '100%', height = '400px', mapType = 'roadmap', hideIfNotFound = false, className = '', ...props }: Props) {
 
   const satelliteFlag = mapType === 'satellite' ? '&t=k' : ''
-  const src = `https://maps.google.com/maps?q=${lat},${lng}&z=${zoom}${satelliteFlag}&output=embed`
+
+  const src = () => {
+    if (coordinates?.lat && coordinates?.lng) {
+      return `https://maps.google.com/maps?q=${coordinates.lat},${coordinates.lng}&z=${zoom}${satelliteFlag}&output=embed`
+    } else {
+      return `https://maps.google.com/maps?z=${zoom}${satelliteFlag}&output=embed`
+    }
+  }
+
+  if (hideIfNotFound && (!coordinates || !coordinates.lat || !coordinates.lng)) {
+    return null
+  }
 
   return (
     <iframe
       width={width}
       height={height}
-      src={src}
+      src={src()}
       allowFullScreen
       loading='lazy'
       referrerPolicy='no-referrer-when-downgrade'
-      className={`rounded ${className}`}
+      className={twMerge(
+        'rounded border-none',
+        className
+      )}
       {...props}
     />
   )
