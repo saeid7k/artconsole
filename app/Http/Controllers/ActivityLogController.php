c8985eeb6fa14c4c6c0c577771a9b4cb2128f335
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-18 13:32:32 -0500

    add log_name to modelActivities method

--- app/Http/Controllers/ActivityLogController.php
+++ app/Http/Controllers/ActivityLogController.php
@@ -8,12 +8,25 @@ use Spatie\Activitylog\Models\Activity;
 class ActivityLogController extends Controller
 {
   public function modelActivities(Request $request) {
+    $validated = $request->validate([
+      'model_type' => ['required', 'string'],
+      'model_id' => ['required', 'integer'],
+      'name' => ['sometimes', 'string'],
+    ]);
+
     $model = ucfirst($request->input('model_type'));
     $modelId = $request->input('model_id');
 
     $activities = Activity::where('subject_type', 'App\\Models\\' . $model)
       ->where('subject_id', $modelId)
-      ->with('causer')
+      ->where(function ($q) use ($request) {
+        if ($request->has('name')) {
+          $q->where('log_name', $request->input('name'));
+        }
+      })
+      ->with(['causer' => function ($q) {
+        $q->select('id', 'firstname', 'lastname', 'email');
+      }])
       ->orderBy('created_at', 'asc')
       ->get();
 
