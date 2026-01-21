7f087bc5f8e9318d6331c4b8b60e295b5cad9b95
Saeid Khajevand <saeid.kh7@gmail.com>
2026-01-21 16:10:47 -0500

    create useLocations hook

---
+++ resources/js/hooks/useLocations.ts
@@ -0,0 +1,29 @@
+import { LocationProps } from "@/types/location";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+function useLocations({ enableQuery = true }) {
+
+  const locationsQuery = useQuery<LocationProps[]>({
+    queryKey: ['locations-query'],
+    queryFn: () =>
+      axios.get(route('locations.options'))
+        .then(response => response.data.locations),
+    enabled: enableQuery,
+  });
+
+  const locationsOptions = (locationsQuery.data)?.map((loc) => ({
+    label: loc.name + (loc.is_primary ? ' (Primary)' : ''),
+    value: loc.id,
+  })) || [];
+
+  const defaultLocationValue = locationsQuery.data?.find(l => l.is_primary)?.id;
+
+  return {
+    locationsQuery,
+    locationsOptions,
+    defaultLocationValue,
+  };
+}
+
+export default useLocations;
