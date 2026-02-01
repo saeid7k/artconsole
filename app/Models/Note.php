db46f453c02055a62c16a32869e05f6cbae8ecf7
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-01 16:35:18 -0500

    remove unused traits from note model

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,21 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Traits\LogsActivity;
 
 class Note extends Model
 {
-  use HasFactory, LogsActivity, SoftDeletes;
-
   protected $fillable = [
-    'note',
     'user_id',
+    'note',
   ];
 
   // Relationships
@@ -30,27 +24,5 @@ class Note extends Model
   {
     return $this->belongsToMany(Artwork::class, 'artwork_note')
       ->withTimestamps();
-  }
-
-  // Activity Log
-
-  public function getActivitylogOptions(): LogOptions
-  {
-    return LogOptions::defaults()
-      ->logFillable()
-      ->logOnlyDirty()
-      ->dontSubmitEmptyLogs()
-      ->setDescriptionForEvent(function (string $event) {
-        switch ($event) {
-          case 'created':
-            return "created a note";
-          case 'updated':
-            return "updated note";
-          case 'deleted':
-            return "deleted a note";
-          default:
-            return $event;
-        }
-      });
   }
 }
