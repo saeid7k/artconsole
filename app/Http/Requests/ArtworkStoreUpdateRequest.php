dc34b23c5d332d55c01151cc78262fa255fd58c8
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-01 15:27:14 -0500

    add provenance field to ArtworkStoreUpdateRequest validation rules

--- app/Http/Requests/ArtworkStoreUpdateRequest.php
+++ app/Http/Requests/ArtworkStoreUpdateRequest.php
@@ -47,6 +47,7 @@ class ArtworkStoreUpdateRequest extends FormRequest
 
       'ownership' => ['nullable', 'string', 'in:owned,consigned'],
       'owner_contact_id' => ['nullable', 'integer', 'exists:contacts,id'],
+      'provenance' => ['nullable', 'string', 'max:20000'],
 
       'details' => ['nullable', 'array'],
       'status' => ['nullable', 'string', 'in:' . ArtworkStatus::stringifyAll()],
