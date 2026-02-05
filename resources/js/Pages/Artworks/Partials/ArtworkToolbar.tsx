0d8bdc72b936360fd43d6783b671b7ac0232d29f
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-04 20:48:43 -0500

    add copy api call to artwork toolbar and reorganize responsive action menu

--- resources/js/Pages/Artworks/Partials/ArtworkToolbar.tsx
+++ resources/js/Pages/Artworks/Partials/ArtworkToolbar.tsx
@@ -3,7 +3,7 @@ import MoveModal from "@/Components/MoveModal";
 import useFilesUpload from "@/hooks/useFilesUpload";
 import { useWindow } from "@/hooks/useWindow";
 import { ArtworkProps } from "@/types/artwork";
-import { ArrowDataTransferHorizontalIcon, Delete02Icon, ImageAddIcon, MoreHorizontalCircle01Icon, PencilEdit02Icon } from "@hugeicons/core-free-icons";
+import { ArrowDataTransferHorizontalIcon, CopyIcon, Delete02Icon, ImageAddIcon, MoreHorizontalCircle01Icon, PencilEdit02Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { router } from "@inertiajs/react";
 import { useMutation } from "@tanstack/react-query";
@@ -45,124 +45,155 @@ function ArtworkToolbar({ artwork }: Props) {
     },
   })
 
+  // Confirm Delete
+
+  const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
+
+  // Copy Mutation
+
+  const copyMutation = useMutation({
+    mutationFn: (artworkId: number) => axios.post(route('artworks.copy', artworkId)),
+    onSuccess: (response: any) => {
+      message.success('Artwork copied successfully')
+      router.visit(route('artworks.show', response.data.artwork_id))
+    },
+    onError: (error: any) => {
+      message.error(error.response?.data?.message || 'Failed to copy artwork')
+    },
+  })
+
   // Render More Actions Menu
 
   const renderMoreActionsMenu = () => {
     return (
       <Menu>
+        {windowWidth < 1024 && (
+          <>
+            <Menu.Item
+              key="add-images"
+              disabled={!artwork.abilities.update}
+              onClick={() => triggerFilesSelect()}
+            >
+              <FlexBox>
+                <HugeiconsIcon icon={ImageAddIcon} size={20} />
+                Add Images
+              </FlexBox>
+            </Menu.Item>
+            <Menu.Item
+              key="edit"
+              disabled={!artwork.abilities.update}
+              onClick={() => setShowEditDrawer(true)}
+            >
+              <FlexBox>
+                <HugeiconsIcon icon={PencilEdit02Icon} size={20} />
+                Edit Artwork
+              </FlexBox>
+            </Menu.Item>
+            <Menu.Item
+              key="move"
+              disabled={!artwork.abilities.update}
+              onClick={() => setOpenMoveModal(true)}
+            >
+              <FlexBox>
+                <HugeiconsIcon icon={ArrowDataTransferHorizontalIcon} size={20} />
+                Move to new Location
+              </FlexBox>
+            </Menu.Item>
+          </>
+        )}
         <Menu.Item
-          key="add-images"
-          disabled={!artwork.abilities.update}
-          onClick={() => triggerFilesSelect()}
+          key="copy"
+          disabled={!artwork.abilities.create}
+          onClick={() => copyMutation.mutate(artwork.id)}
         >
           <FlexBox>
-            <HugeiconsIcon icon={ImageAddIcon} size={20} />
-            Add Images
+            <HugeiconsIcon icon={CopyIcon} size={20} />
+            Copy Artwork
           </FlexBox>
         </Menu.Item>
-        <Menu.Item
-          key="edit"
-          disabled={!artwork.abilities.update}
-          onClick={() => setShowEditDrawer(true)}
-        >
-          <FlexBox>
-            <HugeiconsIcon icon={PencilEdit02Icon} size={20} />
-            Edit Artwork
-          </FlexBox>
-        </Menu.Item>
-        <Menu.Item
-          key="move"
-          disabled={!artwork.abilities.update}
-          onClick={() => setOpenMoveModal(true)}
-        >
-          <FlexBox>
-            <HugeiconsIcon icon={ArrowDataTransferHorizontalIcon} size={20} />
-            Move to new Location
-          </FlexBox>
-        </Menu.Item>
-        <Menu.Item
-          key="delete"
-          disabled={!artwork.abilities.delete}
-          onClick={() => setShowDeleteConfirm(true)}
-        >
-          <FlexBox>
-            <HugeiconsIcon icon={Delete02Icon} size={20} />
-            Delete Artwork
-          </FlexBox>
-        </Menu.Item>
+        {windowWidth < 1024 && (
+          <>
+            <Menu.Item
+              key="delete"
+              disabled={!artwork.abilities.delete}
+              onClick={() => setShowDeleteConfirm(true)}
+            >
+              <FlexBox>
+                <HugeiconsIcon icon={Delete02Icon} size={20} />
+                Delete Artwork
+              </FlexBox>
+            </Menu.Item>
+          </>
+        )}
       </Menu>
     )
   }
 
-  // Confirm Delete
-
-  const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
-
   return (
     <>
-      {windowWidth >= 1024 && (
-        <FlexBox>
-          <Tooltip title="Add Images" mouseEnterDelay={0.5} >
-            <Button
-              type="text"
-              shape="square"
-              onClick={() => triggerFilesSelect()}
-              disabled={!artwork.abilities.update}
-            >
-              <HugeiconsIcon icon={ImageAddIcon} size={20} />
-              {windowWidth >= 1280 && <div>Add Images</div>}
-            </Button>
-          </Tooltip>
-          <Tooltip title="Edit Artwork" mouseEnterDelay={0.5} >
-            <Button
-              type="text"
-              shape="square"
-              onClick={() => setShowEditDrawer(true)}
-              disabled={!artwork.abilities.update}
-            >
-              <HugeiconsIcon icon={PencilEdit02Icon} size={20} />
-              {windowWidth >= 1280 && <div>Edit</div>}
-            </Button>
-          </Tooltip>
-          <Tooltip title="Move to new Location" mouseEnterDelay={0.5} >
-            <Button
-              type="text"
-              shape="square"
-              onClick={() => setOpenMoveModal(true)}
-              disabled={!artwork.abilities.update}
-            >
-              <HugeiconsIcon icon={ArrowDataTransferHorizontalIcon} size={20} />
-              {windowWidth >= 1280 && <div>Move</div>}
-            </Button>
-          </Tooltip>
-          <Tooltip title="Delete Artwork" mouseEnterDelay={0.5} >
-            <Popconfirm
-              title="Delete the artwork"
-              description={
-                <div>
-                  Are you sure to delete this artwork?
-                  <div className="italic text-red-500">{artwork.title}</div>
-                </div>
-              }
-              onConfirm={() => deleteMutation.mutate(artwork.id)}
-              okText="Yes"
-              cancelText="No"
-              placement="left"
-              okType="danger"
-            >
+      <FlexBox>
+        {windowWidth >= 1024 && (
+          <>
+            <Tooltip title="Add Images" mouseEnterDelay={0.5} >
               <Button
                 type="text"
                 shape="square"
-                disabled={!artwork.abilities.delete}
+                onClick={() => triggerFilesSelect()}
+                disabled={!artwork.abilities.update}
               >
-                <HugeiconsIcon icon={Delete02Icon} size={20} />
-                {windowWidth >= 1280 && <div>Delete</div>}
+                <HugeiconsIcon icon={ImageAddIcon} size={20} />
+                {windowWidth >= 1280 && <div>Add Images</div>}
               </Button>
-            </Popconfirm>
-          </Tooltip>
-        </FlexBox>
-      )}
-      {windowWidth < 1024 && (
+            </Tooltip>
+            <Tooltip title="Edit Artwork" mouseEnterDelay={0.5} >
+              <Button
+                type="text"
+                shape="square"
+                onClick={() => setShowEditDrawer(true)}
+                disabled={!artwork.abilities.update}
+              >
+                <HugeiconsIcon icon={PencilEdit02Icon} size={20} />
+                {windowWidth >= 1280 && <div>Edit</div>}
+              </Button>
+            </Tooltip>
+            <Tooltip title="Move to new Location" mouseEnterDelay={0.5} >
+              <Button
+                type="text"
+                shape="square"
+                onClick={() => setOpenMoveModal(true)}
+                disabled={!artwork.abilities.update}
+              >
+                <HugeiconsIcon icon={ArrowDataTransferHorizontalIcon} size={20} />
+                {windowWidth >= 1280 && <div>Move</div>}
+              </Button>
+            </Tooltip>
+            <Tooltip title="Delete Artwork" mouseEnterDelay={0.5} >
+              <Popconfirm
+                title="Delete the artwork"
+                description={
+                  <div>
+                    Are you sure to delete this artwork?
+                    <div className="italic text-red-500">{artwork.title}</div>
+                  </div>
+                }
+                onConfirm={() => deleteMutation.mutate(artwork.id)}
+                okText="Yes"
+                cancelText="No"
+                placement="left"
+                okType="danger"
+              >
+                <Button
+                  type="text"
+                  shape="square"
+                  disabled={!artwork.abilities.delete}
+                >
+                  <HugeiconsIcon icon={Delete02Icon} size={20} />
+                  {windowWidth >= 1280 && <div>Delete</div>}
+                </Button>
+              </Popconfirm>
+            </Tooltip>
+          </>
+        )}
         <Dropdown
           trigger={['click']}
           placement="bottomLeft"
@@ -175,7 +206,7 @@ function ArtworkToolbar({ artwork }: Props) {
             <HugeiconsIcon icon={MoreHorizontalCircle01Icon} size={20} />
           </Button>
         </Dropdown>
-      )}
+      </FlexBox>
 
       {FilesInput}
 
