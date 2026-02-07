80b7b3f3722a53a7d3472114adbf3e344f92439a
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-07 13:41:50 -0500

    add add and delete tag functionality to manage tags modal

--- resources/js/Components/ManageTagsModal.tsx
+++ resources/js/Components/ManageTagsModal.tsx
@@ -1,11 +1,10 @@
 import { ucFirst } from "@/utils/stringHelper";
-import { useQuery } from "@tanstack/react-query";
-import { Button, Input, Modal, Space } from "antd";
-import axios from "axios";
-import FlexBox from "./Containers/FlexBox";
-import { Group } from "antd/es/radio";
+import { Delete02Icon, FloppyDiskIcon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { FloppyDiskIcon } from "@hugeicons/core-free-icons";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Button, Input, message, Modal, Space } from "antd";
+import axios from "axios";
+import { useState } from "react";
 
 type Props = {
   open?: boolean;
@@ -33,14 +32,69 @@ function ManageTagsModal({ open , setOpen, type = 'tag' }: Props) {
     enabled: open ? true : false,
   })
 
+  // Add new tag
+
+  const [newTag, setNewTag] = useState<string | null>(null);
+
+  const addTagMutation = useMutation({
+    mutationFn: (newTag: string) => axios.post(route('tags.store'), {
+        type,
+        value: newTag,
+      }),
+    onSuccess: () => {
+      tagsQuery.refetch();
+      setNewTag(null);
+      message.success(`New ${type} added successfully`);
+    },
+    onError: (err: any) => {
+      message.error(err.response?.data?.message || `Failed to add new ${type}`);
+    }
+  })
+
+  // Delete tag
+
+  const deleteTagMutation = useMutation({
+    mutationFn: (tagId: number) => axios.delete(route('tags.destroy', { tag: tagId })),
+    onSuccess: () => {
+      tagsQuery.refetch();
+      message.success(`${ucFirst(type)} deleted successfully`);
+    },
+    onError: (err: any) => {
+      message.error(err.response?.data?.message || `Failed to delete ${type}`);
+    }
+  })
+
   return (
-    <Modal open={open} onCancel={handleClose} title={`Manage ${ucFirst(type)}s`}>
+    <Modal
+      open={open}
+      onCancel={handleClose}
+      title={`Manage ${ucFirst(type)}s`}
+      footer={null}
+    >
       <div className="flex flex-col gap-3 my-3">
         <div className="flex flex-col gap-1 max-h-[300px] overflow-y-auto" >
           {tagsQuery.isFetched && tagsQuery.data.length > 0 && (
             tagsQuery.data.map((tag: any) => (
-              <div className="p-2 bg-light border rounded-lg">
+              <div
+                key={tag.id}
+                className="flex items-center justify-between p-2 bg-light border rounded-lg"
+              >
                 {tag.value}
+                <div>
+                  {tag.gallery_id && (
+                    <Button
+                      type="text"
+                      danger
+                      shape="circle"
+                      onClick={() => deleteTagMutation.mutate(tag.id)}
+                    >
+                      <HugeiconsIcon icon={Delete02Icon} size={16} />
+                    </Button>
+                  )}
+                  {!tag.gallery_id && (
+                    <div className="text-ghost ">System Default</div>
+                  )}
+                </div>
               </div>
             ))
           )}
@@ -49,19 +103,21 @@ function ManageTagsModal({ open , setOpen, type = 'tag' }: Props) {
           <Input
             placeholder={`Add new ${type}...`}
             size="large"
+            value={newTag || ''}
+            onChange={(e) => setNewTag(e.target.value)}
           />
           <Button
             size="large"
+            onClick={() => {
+              if (newTag && newTag.trim() !== '') {
+                addTagMutation.mutate(newTag);
+              }
+            }}
           >
             <HugeiconsIcon icon={FloppyDiskIcon} size={20} />
           </Button>
         </Space.Compact>
       </div>
-
-
-        <pre className="bg-yellow-100">
-          {JSON.stringify(tagsQuery.data, null, 2)}
-        </pre>
     </Modal>
   )
 }
