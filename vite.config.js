d57d7f720ce644e9f6f9d087af47703d77deba09
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-07 18:22:48 -0500

    add alias for CSS resources in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'resources/js'),
-      '~': path.resolve(__dirname) // Alias for root dir
+      '~': path.resolve(__dirname),
+      '@css': path.resolve(__dirname, 'resources/css'),
     }
   }
 });
