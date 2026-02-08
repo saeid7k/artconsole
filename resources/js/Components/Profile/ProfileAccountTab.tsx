033be1080e6917fe5d28bd0fb20d5e1efe65b6dd
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-08 11:30:18 -0500

    create ProfileAccountTab component for managing user passwords

---
+++ resources/js/Components/Profile/ProfileAccountTab.tsx
@@ -0,0 +1,45 @@
+import { AuthProps } from "@/types/auth"
+import { usePage } from "@inertiajs/react"
+import { Button } from "antd"
+import { useState } from "react"
+import ChangePasswordModal from "./ChangePasswordModal"
+
+function ProfileAccountTab() {
+
+  const { user } = usePage().props.auth as AuthProps
+
+  const [showPasswordModal, setShowPasswordModal] = useState(false)
+
+  return (
+    <>
+      <div
+        className="flex flex-col gap-3"
+      >
+        <div className="flex justify-between">
+          <div>
+            <strong>Password</strong>
+            <div className="text-muted">
+              {!user.has_password && (<>You have not set a password yet.</>)}
+              {user.has_password && (<>Change your password regularly to keep your account secure.</>)}
+            </div>
+          </div>
+          <div>
+            <Button
+              onClick={() => setShowPasswordModal(true)}
+            >
+              {user.has_password ? 'Change Password' : 'Set Password'}
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <ChangePasswordModal
+        open={showPasswordModal}
+        setOpen={setShowPasswordModal}
+        action={user.has_password ? 'change' : 'set'}
+      />
+    </>
+  )
+}
+
+export default ProfileAccountTab
