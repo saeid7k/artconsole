7b6940e0364b9aebda114192a33ad62bf16ec1d2
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-08 10:27:21 -0500

    add color prop to LoadingSpinner and dynamic padding

--- resources/js/Components/LoadingSpinner.tsx
+++ resources/js/Components/LoadingSpinner.tsx
@@ -1,13 +1,34 @@
 import { Spin } from "antd";
+import { twMerge } from "tailwind-merge";
 
 type Props = {
   size?: 'small' | 'default' | 'large';
+  color?: 'default' | 'white';
 }
 
-function LoadingSpinner({ size = 'default' }: Props) {
+function LoadingSpinner({ size = 'default', color = 'default' }: Props) {
+
+  const paddingSize = {
+    small: 'p-1',
+    default: 'p-3',
+    large: 'p-5',
+  }
+
   return (
-    <div className="flex justify-center p-5">
-      <Spin size={size} />
+    <div
+      className={twMerge(
+        "flex justify-center",
+        paddingSize[size]
+      )}
+    >
+      <Spin
+        size={size}
+        styles={{
+          indicator: {
+            color: color === 'white' ? "#fff" : undefined
+          }
+        }}
+      />
     </div>
   )
 }
