d64f637ef48a8f1f1b7b2f7d3928a77f521c18a6
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-14 20:40:04 -0500

    add Accounting component for managing gallery accounting settings

---
+++ resources/js/Components/GallerySettings/Accounting.tsx
@@ -0,0 +1,67 @@
+import { CURRENCIES_OPTIONS } from "@/constants/currencies"
+import { useGallerySettings } from "@/contexts/GallerySettingsContext"
+import { Form, message, Select } from "antd"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import CONFIGS from "~/resources/configs.json"
+import ActionFooter from "../ActionFooter"
+
+function Accounting() {
+
+  const { open, gallery } = useGallerySettings()
+  const [form] = Form.useForm()
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  function handleSave() {
+    form
+      .validateFields()
+      .then(values => {
+        setIsProcessing(true);
+        axios.post(route('galleries.update-accounting', { gallery: gallery.id }), values)
+          .then((response) => {
+            message.success(response.data.message || 'Accounting settings updated successfully.');
+          })
+          .catch(error => {
+            message.error(error.response?.data?.message || 'Failed to update accounting settings.');
+          })
+          .finally(() => {
+            setIsProcessing(false);
+          });
+    });
+  }
+
+  // Effects
+
+  useEffect(() => {
+    if (open) {
+      form.resetFields()
+    }
+  }, [open]);
+
+  return (
+    <div>
+      <Form
+        form={form}
+        layout="vertical"
+        initialValues={{
+          currency: gallery?.meta?.currency || CONFIGS.defaults.currency,
+        }}
+        validateTrigger="onSubmit"
+      >
+        <Form.Item label="Currency" name="currency">
+          <Select
+            options={CURRENCIES_OPTIONS}
+            showSearch
+            defaultValue={CONFIGS.defaults.currency}
+          />
+        </Form.Item>
+      </Form>
+      <ActionFooter
+        save={handleSave}
+        isProcessing={isProcessing}
+      />
+    </div>
+  )
+}
+
+export default Accounting
