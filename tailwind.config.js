042e31d4dc80c3575a32235c2bad55348a712be9
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-18 17:02:34 -0500

    add primary-light color variable to theme and update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,6 +48,7 @@ export default {
     			primary: {
             DEFAULT: 'var(--primary)',
             foreground: 'var(--primary-foreground)',
+            light: 'var(--primary-light)',
             50: 'var(--primary-50)',
             100: 'var(--primary-100)',
             200: 'var(--primary-200)',
