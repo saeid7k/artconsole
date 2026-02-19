4e62e976a895d79c235cbd09bdd5e3c0ad60ab38
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-18 23:51:05 -0500

    add store method to ReportController and define route for report creation

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\ReportType;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
@@ -9,5 +10,30 @@ class ReportController extends Controller
   public function index()
   {
     return inertia('Reports/Index');
+  }
+
+  public function store(Request $request)
+  {
+    $user = auth()->user();
+    $gallery = $user->currentGallery();
+
+    $request->validate([
+      'type' => 'required|string|in:' . ReportType::stringifyAll(),
+      'name' => 'required|string',
+      'description' => 'nullable|string',
+      'options' => 'nullable|object',
+      'artworks' => 'nullable|array',
+    ]);
+
+    $report = $gallery->reports()->create([
+      'user_id' => $user->id,
+      'type' => $request->type,
+      'name' => $request->name,
+      'description' => $request->description ?? null,
+      'options' => $request->options ?? null,
+      'artworks' => $request->artworks ?? null,
+    ]);
+
+    return redirect()->route('reports.index', $report);
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -117,6 +117,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
   Route::prefix('reports')->name('reports.')->group(function () {
     Route::get('/', [App\Http\Controllers\ReportController::class, 'index'])->name('index');
+    Route::post('/store', [App\Http\Controllers\ReportController::class, 'store'])->name('store');
   });
 });
 
