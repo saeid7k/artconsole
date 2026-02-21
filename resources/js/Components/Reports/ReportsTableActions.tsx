28a2e4fd8a7f647f53a64147c233a1adb350974e
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-21 18:39:25 -0500

    create ReportPreviewDrawer component for report preview functionality

--- resources/js/Components/Reports/ReportsTableActions.tsx
+++ resources/js/Components/Reports/ReportsTableActions.tsx
@@ -1,46 +1,53 @@
-import { downloadFile } from "@/utils/downloadHelper"
+import useReport from "@/hooks/useReport"
 import { Delete02Icon, FileViewIcon, PdfIcon } from "@hugeicons/core-free-icons"
 import { HugeiconsIcon } from "@hugeicons/react"
 import { Button, Tooltip } from "antd"
+import { useState } from "react"
 import FlexBox from "../Containers/FlexBox"
+import ReportPreviewDrawer from "./ReportPreviewDrawer"
 
 function ReportsTableActions({ report }: any) {
 
-  function handleDownload() {
-    downloadFile({
-      url: route('reports.download', { report: report.id }),
-      fileName: report.name + '.pdf'
-    })
-  }
+  const [showPreview, setShowPreview] = useState(false)
+  const { handleDownload } = useReport(report);
 
   return (
-    <FlexBox>
-      <Tooltip title="Preview">
-        <Button
-          variant="text"
-          color='blue'
-          shape="circle"
-          icon={<HugeiconsIcon icon={FileViewIcon} size={20} />}
-        />
-      </Tooltip>
-      <Tooltip title="Download PDF">
-        <Button
-          variant="text"
-          color='red'
-          shape="circle"
-          icon={<HugeiconsIcon icon={PdfIcon} size={20} />}
-          onClick={handleDownload}
-        />
-      </Tooltip>
-      <Tooltip title="Delete">
-        <Button
-          variant="text"
-          color='danger'
-          shape="circle"
-          icon={<HugeiconsIcon icon={Delete02Icon} size={20} />}
-        />
-      </Tooltip>
-    </FlexBox>
+    <>
+      <FlexBox>
+        <Tooltip title="Preview">
+          <Button
+            variant="text"
+            color='blue'
+            shape="circle"
+            icon={<HugeiconsIcon icon={FileViewIcon} size={20} />}
+            onClick={() => setShowPreview(true)}
+          />
+        </Tooltip>
+        <Tooltip title="Download PDF">
+          <Button
+            variant="text"
+            color='red'
+            shape="circle"
+            icon={<HugeiconsIcon icon={PdfIcon} size={20} />}
+            onClick={handleDownload}
+          />
+        </Tooltip>
+        <Tooltip title="Delete">
+          <Button
+            variant="text"
+            color='danger'
+            shape="circle"
+            icon={<HugeiconsIcon icon={Delete02Icon} size={20} />}
+          />
+        </Tooltip>
+      </FlexBox>
+
+      <ReportPreviewDrawer
+        report={report}
+        show={showPreview}
+        onClose={() => setShowPreview(false)}
+      />
+    </>
   )
 }
 
