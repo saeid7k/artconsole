af0c65d1e763093bed868509bec44a1d7d97abcd
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-21 12:24:39 -0500

    update label report pdf blade file

--- resources/views/reports/artworks-label-small.blade.php
+++ resources/views/reports/artworks-label-small.blade.php
@@ -3,36 +3,55 @@
 
 <head>
   <style>
-    .label-wrapper {
+
+    html, body {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    .labels-container {
       display: flex;
       flex-direction: column;
       justify-content: start;
       align-items: start;
+      align-content: flex-start;
       width: 100%;
       height: 10in;
       flex-wrap: wrap;
       gap: 0 0.125in;
+      box-sizing: content-box;
     }
+
     .label {
       width: 2.625in;
-      height: 1in; /* 1 inch */
-      border: 1pt solid #000;
+      height: 1in;
       font-size: 10pt;
       box-sizing: border-box;
       border-radius: 8pt;
       padding: 0.125in;
+      break-inside: avoid;
+      page-break-inside: avoid;
+      border: 1pt solid #0000;
+    }
+
+    .label-border {
+      border: 1pt solid #000 !important;
     }
   </style>
 </head>
 
 <body>
-  <div class="report-wrapper">
-    <div class="label-wrapper">
-      @foreach ($artworks as $artwork)
-          <div class="label">
-            Name: {{ $artwork['title'] }}
-          </div>
+  @foreach (collect($artworks)->chunk(30) as $chunk)
+    <div class="labels-container @if(!$loop->last) page-break @endif">
+      @foreach ($chunk as $artwork)
+        <div @class([
+          'label',
+          'label-border' => $report->options->border ?? false,
+        ])>
+          Name: {{ $artwork->title }}
+        </div>
       @endforeach
     </div>
-  </div>
+  @endforeach
 </body>
