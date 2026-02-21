46d08c34d363b41a4044917d7b2e08c42626e1ab
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-21 12:24:17 -0500

    add footer blade view for report pagination

---
+++ resources/views/reports/footer.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .footer-container {
+            width: 100%;
+            display: flex;
+            justify-content: end;
+            font-size: 10pt;
+            font-family: sans-serif;
+            padding: 0 0.1875in;
+            color: #777;
+        }
+        .page-number {
+            text-align: right;
+        }
+    </style>
+</head>
+<body>
+    <div class="footer-container">
+      <div class="page-number">
+        Page @pageNumber of @totalPages
+      </div>
+    </div>
+</body>
+</html>
