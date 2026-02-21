840cad0dc0769a9939e1c41c120f651e67f94f81
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-21 12:25:30 -0500

    create ReportService and generatePdf method to generate pdf in report store method

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Enums\ReportType;
+use App\Services\ReportService;
 use Illuminate\Http\Request;
-use Spatie\LaravelPdf\Enums\Format;
-use Spatie\LaravelPdf\Enums\Unit;
 
 use function Spatie\LaravelPdf\Support\pdf;
 
@@ -46,57 +45,15 @@ class ReportController extends Controller
       'artworks' => $request->artworks ?? null,
     ]);
 
-    return redirect()->route('reports.index', $report);
+    (new ReportService($report))->generatePdf();
+
+    return redirect()->route('reports.index');
   }
 
   public function download(Request $request, $reportId)
   {
-    $user = auth()->user();
-    $gallery = $user->currentGallery();
-
-    $report = $gallery->reports()->findOrFail($reportId);
-
-    switch ($report->type) {
-      case 'artworks_label':
-        $viewPath = 'reports.artworks-label';
-        break;
-      case 'inventory_report':
-        $viewPath = 'reports.inventory-report';
-        break;
-    }
-
-    switch ($report->options->size ?? null) {
-      case 'small':
-        $viewPath = $viewPath . '-small';
-        $margins = [0.5, 0.1875, 0.5, 0.1875];
-        break;
-      case 'medium':
-        $viewPath = $viewPath . '-medium';
-        $margins = [0.5, 0.25, 0.5, 0.25];
-        break;
-      case 'large':
-        $viewPath = $viewPath . '-large';
-        $margins = [0.5, 0.5, 0.5, 0.5];
-        break;
-      default:
-        $viewPath = $viewPath . '-small';
-        $margins = [0.5, 0.1875, 0.5, 0.1875];
-    }
-
-    if (!isset($viewPath)) {
-      return response()->json([
-        'message' => 'Invalid report type'
-      ], 400);
-    }
-
-    $artworks = $gallery->artworks()->whereIn('id', $report->artworks)->get()->toArray() ?? [];
-
-    return pdf()
-      ->view($viewPath, ['report' => $report, 'artworks' => [...$artworks, ...$artworks]])
-      ->format(Format::Letter)
-      ->margins($margins[0], $margins[1], $margins[2], $margins[3], Unit::Inch)
-      ->download($report->name . '.pdf');
-
-    abort(404);
+    return response()->json([
+      'message' => 'Not implemented yet'
+    ], 501);
   }
 }
