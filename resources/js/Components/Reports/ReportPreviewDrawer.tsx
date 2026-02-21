a8f23f829fb184e7a677dd03880ce30b09270ea5
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-21 18:39:25 -0500

    create ReportPreviewDrawer component for report preview functionality

---
+++ resources/js/Components/Reports/ReportPreviewDrawer.tsx
@@ -0,0 +1,55 @@
+import useReport from "@/hooks/useReport";
+import { Pdf01Icon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { useQuery } from "@tanstack/react-query";
+import { Button, Divider, Drawer } from "antd";
+import axios from "axios";
+import FlexBox from "../Containers/FlexBox";
+
+type Props = {
+  report: any;
+  show: boolean;
+  onClose: () => void;
+}
+
+function ReportPreviewDrawer({ report, show, onClose }: Props) {
+
+  const { handleDownload } = useReport(report);
+
+  const urlQuery = useQuery({
+    queryKey: ['report-file', report.id],
+    queryFn: () => axios.get(route('reports.url', { report: report.id }))
+      .then(response => response.data)
+      .catch(error => {
+        console.error('Error fetching report URL:', error);
+        return null;
+      }),
+    enabled: show && !!report.id
+  })
+
+  return(
+    <Drawer
+      title={<FlexBox>{report.name}<Divider orientation="vertical" /><div className="text-primary">Preview</div></FlexBox>}
+      placement="right"
+      onClose={onClose}
+      open={show}
+      size={800}
+      extra={
+        <Button
+          variant="text"
+          shape="circle"
+          color="red"
+          onClick={handleDownload}
+          icon={<HugeiconsIcon icon={Pdf01Icon} />}
+        />
+      }
+    >
+      <iframe
+        src={urlQuery.data?.url || ''}
+        style={{ width: '100%', height: '100%' }}
+      />
+    </Drawer>
+  )
+}
+
+export default ReportPreviewDrawer
