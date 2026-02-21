e53e4df7b6b35a31a1a4e2d469aff83cb8bed3bb
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-21 12:25:30 -0500

    create ReportService and generatePdf method to generate pdf in report store method

---
+++ app/Services/ReportService.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Report;
+use Illuminate\Support\Str;
+use Spatie\LaravelPdf\Enums\Format;
+use Spatie\LaravelPdf\Enums\Unit;
+
+use function Spatie\LaravelPdf\Support\pdf;
+
+class ReportService
+{
+  protected $report;
+
+  public function __construct(Report $report)
+  {
+    $this->report = $report;
+  }
+
+  public function generatePdf()
+  {
+    switch ($this->report->type) {
+      case 'artworks_label':
+        $viewPath = 'reports.artworks-label';
+        break;
+      case 'inventory_report':
+        $viewPath = 'reports.inventory-report';
+        break;
+    }
+
+    switch ($this->report->options->size ?? null) {
+      case 'small':
+        $viewPath = $viewPath . '-small';
+        $margins = [0.5, 0.1875, 0.5, 0.1875];
+        break;
+      case 'medium':
+        $viewPath = $viewPath . '-medium';
+        $margins = [0.5, 0.25, 0.5, 0.25];
+        break;
+      case 'large':
+        $viewPath = $viewPath . '-large';
+        $margins = [0.5, 0.5, 0.5, 0.5];
+        break;
+      default:
+        $viewPath = $viewPath . '-small';
+        $margins = [0.5, 0.1875, 0.5, 0.1875];
+    }
+
+    if (!isset($viewPath)) {
+      return response()->json([
+        'message' => 'Invalid report type'
+      ], 400);
+    }
+
+    $gallery = $this->report->gallery;
+    $artworks = $gallery->artworks()->whereIn('id', $this->report->artworks)->get() ?? [];
+    $tempPath = storage_path('app/report_' . $this->report->id . '.pdf');
+
+    pdf()
+      ->view($viewPath, ['report' => $this->report, 'artworks' => [...$artworks, ...$artworks, ...$artworks]])
+      ->format(Format::Letter)
+      ->margins($margins[0], $margins[1], $margins[2], $margins[3], Unit::Inch)
+      ->footerView('reports.footer')
+      ->save($tempPath);
+
+    $this->report->addMedia($tempPath)
+      ->usingFileName(Str::slug($this->report->name) . '.pdf')
+      ->toMediaCollection('report');
+  }
+}
