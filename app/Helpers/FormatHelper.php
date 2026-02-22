5d187529c37d31d0582e4cff20777846b6189492
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-22 00:35:47 -0500

    add formatDimensions and stringifyArray to FormatHelper

--- app/Helpers/FormatHelper.php
+++ app/Helpers/FormatHelper.php
@@ -14,4 +14,43 @@ class FormatHelper
 
     return floatval($cleanedValue);
   }
+
+  public static function formatDimensions($dimensions, bool $showDepth = false): string
+  {
+    if (!$dimensions) {
+      return '-';
+    }
+
+    $d = (array) $dimensions;
+
+    $width  = $d['width']  ?? 0;
+    $height = $d['height'] ?? 0;
+    $depth  = $d['depth']  ?? 0;
+    $unit   = $d['unit']   ?? '';
+
+    $unitSymbol = match ($unit) {
+      'inches' => 'in',
+      'cm'     => 'cm',
+      default  => '',
+    };
+
+    $dimensionString = "{$width} x {$height}";
+
+    if ($showDepth && $depth) {
+      $dimensionString .= " x {$depth}";
+    }
+
+    $dimensionString .= " {$unitSymbol}";
+
+    return $dimensionString;
+  }
+
+  public static function stringifyArray($array): string
+  {
+    if (is_null($array) || empty($array)) {
+      return '-';
+    }
+
+    return implode(', ', $array);
+  }
 }
