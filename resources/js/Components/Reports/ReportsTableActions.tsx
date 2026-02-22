52d2e9c3959babc6decf20c0f011c6225f608323
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-21 19:53:14 -0500

    add delete confirmation dialog to ReportsTableActions component

--- resources/js/Components/Reports/ReportsTableActions.tsx
+++ resources/js/Components/Reports/ReportsTableActions.tsx
@@ -1,7 +1,7 @@
 import useReport from "@/hooks/useReport"
 import { Delete02Icon, FileViewIcon, PdfIcon } from "@hugeicons/core-free-icons"
 import { HugeiconsIcon } from "@hugeicons/react"
-import { Button, Tooltip } from "antd"
+import { Button, Popconfirm, Tooltip } from "antd"
 import { useState } from "react"
 import FlexBox from "../Containers/FlexBox"
 import ReportPreviewDrawer from "./ReportPreviewDrawer"
@@ -9,7 +9,7 @@ import ReportPreviewDrawer from "./ReportPreviewDrawer"
 function ReportsTableActions({ report }: any) {
 
   const [showPreview, setShowPreview] = useState(false)
-  const { handleDownload } = useReport(report);
+  const { handleDownload, deleteMutation } = useReport(report);
 
   return (
     <>
@@ -33,12 +33,27 @@ function ReportsTableActions({ report }: any) {
           />
         </Tooltip>
         <Tooltip title="Delete">
-          <Button
-            variant="text"
-            color='danger'
-            shape="circle"
-            icon={<HugeiconsIcon icon={Delete02Icon} size={20} />}
-          />
+          <Popconfirm
+            title="Delete the report"
+            description={
+              <div>
+                Are you sure to delete this report?
+                <div className="italic text-red-500">{report.name}</div>
+              </div>
+            }
+            onConfirm={() => deleteMutation.mutate()}
+            okText="Yes"
+            cancelText="No"
+            placement="left"
+            okType="danger"
+          >
+            <Button
+              variant="text"
+              color='danger'
+              shape="circle"
+              icon={<HugeiconsIcon icon={Delete02Icon} size={20} />}
+            />
+          </Popconfirm>
         </Tooltip>
       </FlexBox>
 
