f5c1967c2ebc124df20a9709bdf7fcb100fec976
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-27 17:09:46 -0500

    Replace dayjs with dayjsUserTz for user-specific timezone formatting in StyledDate component

--- resources/js/Components/StyledDate.tsx
+++ resources/js/Components/StyledDate.tsx
@@ -1,6 +1,6 @@
+import { dayjsUserTz } from "@/utils/dateTimeHelper"
 import { Calendar03Icon } from "@hugeicons/core-free-icons"
 import { HugeiconsIcon } from "@hugeicons/react"
-import dayjs from "dayjs"
 import FlexBox from "./Containers/FlexBox"
 
 type Props = {
@@ -12,8 +12,8 @@ function StyledDate({ value, showIcon = true }: Props) {
   return (
     <FlexBox>
       {showIcon && <HugeiconsIcon icon={Calendar03Icon} size={20} className="text-muted" />}
-      <div>{dayjs(value).format('MMM D ,YYYY')}</div>
-      <div className="text-muted">{dayjs(value).format('h:mm A')}</div>
+      <div>{dayjsUserTz(value).format('MMM D ,YYYY')}</div>
+      <div className="text-muted">{dayjsUserTz(value).format('h:mm A')}</div>
     </FlexBox>
   )
 }
