7a986738b2d68bc555a5973e5c94f5ab2534013c
Saeid Khajevand <saeid.kh7@gmail.com>
2026-02-28 21:46:57 -0500

    create ARTWORK_DOCUMENTS const and add Certificate of Authenticity document with a sample thumbnail

---
+++ resources/js/constants/artworkDocuments.ts
@@ -0,0 +1,11 @@
+import coaThumbnail from '~/resources/documents/coa/coa-thumbnail.png';
+
+const ARTWORK_DOCUMENTS = [
+  {
+    label: 'Certificate of Authenticity',
+    value: 'coa',
+    thumbnailUrl: coaThumbnail,
+  },
+]
+
+export { ARTWORK_DOCUMENTS };
