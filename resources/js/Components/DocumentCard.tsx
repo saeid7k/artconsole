f360f5ec29e0c916ef2756d45ac3cf437fc230d3
Saeid Khajevand <saeid.kh7@gmail.com>
2026-03-04 22:54:39 -0500

    add DocumentPreviewDrawer to DocumentCard

--- resources/js/Components/DocumentCard.tsx
+++ resources/js/Components/DocumentCard.tsx
@@ -1,55 +1,61 @@
 import colors from "@/Themes/theme";
+import { DocumentProps } from "@/types/document";
 import { Download01Icon, FileViewIcon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { Button, Card, Tooltip } from "antd";
+import { useState } from "react";
+import DocumentPreviewDrawer from "./DocumentPreviewDrawer";
 
-type Props = {
-  document: {
-    label: string;
-    value: string;
-    thumbnailUrl?: string;
-  };
-}
+function DocumentCard({ document }: { document: DocumentProps }) {
 
-function DocumentCard({ document }: Props) {
+  const [showPreview, setShowPreview] = useState(false);
+
   return (
-    <Card
-      size="small"
-      title={document.label}
-      actions={[
-        <Tooltip title="Preview" placement="bottom" mouseEnterDelay={1} >
-          <Button
-            variant="text"
-            color="blue"
-            shape="circle"
-            // onClick={() => }
-          >
-            <HugeiconsIcon icon={FileViewIcon} size={20} />
-          </Button>
-        </Tooltip>,
-        <Tooltip title="Download" placement="bottom" mouseEnterDelay={1} >
-          <Button
-            variant="text"
-            color="purple"
-            shape="circle"
-            // onClick={() => }
-          >
-            <HugeiconsIcon icon={Download01Icon} size={20} />
-          </Button>
-        </Tooltip>,
-      ]}
-      styles={{
-        body: {
-          backgroundColor: colors.gray[50],
-        }
-      }}
-    >
-      <img
-        src={document.thumbnailUrl}
-        alt={document.label}
-        className="w-[150px] h-[150px] object-cover rounded shadow"
+    <>
+      <Card
+        size="small"
+        title={document.label}
+        actions={[
+          <Tooltip title="Preview" placement="bottom" mouseEnterDelay={1} >
+            <Button
+              variant="text"
+              color="blue"
+              shape="circle"
+              onClick={() => setShowPreview(true)}
+            >
+              <HugeiconsIcon icon={FileViewIcon} size={20} />
+            </Button>
+          </Tooltip>,
+          <Tooltip title="Download" placement="bottom" mouseEnterDelay={1} >
+            <Button
+              variant="text"
+              color="purple"
+              shape="circle"
+              // onClick={() => }
+            >
+              <HugeiconsIcon icon={Download01Icon} size={20} />
+            </Button>
+          </Tooltip>,
+        ]}
+        styles={{
+          body: {
+            backgroundColor: colors.gray[50],
+          }
+        }}
+      >
+        <img
+          src={document.thumbnailUrl}
+          alt={document.label}
+          className="w-[150px] h-[150px] object-cover rounded shadow"
+        />
+      </Card>
+
+      <DocumentPreviewDrawer
+        document={document}
+        show={showPreview}
+        onClose={() => setShowPreview(false)}
       />
-    </Card>
+    </>
   )
 }
 
