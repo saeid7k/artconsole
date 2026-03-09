f0505291ed19c1323d9accf76ed3bb97cc40ef7d
Saeid Khajevand <saeid.kh7@gmail.com>
2026-03-09 00:15:45 -0400

    add Taxes component and integrate it into Accounting settings

--- resources/js/Components/GallerySettings/Accounting.tsx
+++ resources/js/Components/GallerySettings/Accounting.tsx
@@ -1,20 +1,23 @@
 import { Tabs } from "antd"
 import Currency from "./Accounting/Currency"
+import Taxes from "./Accounting/Taxes"
 
 function Accounting() {
 
   return (
     <div>
       <Tabs
+        defaultActiveKey="taxes"
         items={[
+          {
+            key: 'taxes',
+            label: 'Taxes',
+            children: <Taxes />
+          },
           {
             key: 'currency',
             label: 'Currency',
             children: <Currency />
-          },
-          {
-            key: 'taxes',
-            label: 'Taxes',
           },
         ]}
       />
