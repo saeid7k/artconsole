ddc8b4fd626f2752e8dbc1ad8d06e908e8a1099d
Saeid Khajevand <saeid.kh7@gmail.com>
2026-03-10 21:43:20 -0400

    add TaxFormModal integration and update tax entries display

--- resources/js/Components/GallerySettings/Accounting/Taxes.tsx
+++ resources/js/Components/GallerySettings/Accounting/Taxes.tsx
@@ -1,7 +1,10 @@
 import FlexBox from "@/Components/Containers/FlexBox"
-import { Delete02Icon, Edit03Icon, StarIcon } from "@hugeicons/core-free-icons"
+import { TaxProps } from "@/types/tax"
+import { AddIcon, Delete02Icon, Edit03Icon, StarIcon } from "@hugeicons/core-free-icons"
 import { HugeiconsIcon } from "@hugeicons/react"
 import { Button, Tag, Tooltip } from "antd"
+import { useState } from "react"
+import TaxFormModal from "./TaxFormModal"
 
 function Taxes() {
 
@@ -9,6 +12,7 @@ function Taxes() {
   const taxes = [
     {
       id: 1,
+      gallery_id: 1,
       name: "HST/GST",
       abbreviation: "HST",
       description: "Harmonized Sales Tax / Goods and Services Tax",
@@ -18,6 +22,7 @@ function Taxes() {
     },
     {
       id: 2,
+      gallery_id: 1,
       name: "PST",
       abbreviation: "PST",
       description: "Provincial Sales Tax",
@@ -27,6 +32,7 @@ function Taxes() {
     },
     {
       id: 3,
+      gallery_id: 1,
       name: "Indian Act Tax Exemption",
       abbreviation: "IAT",
       description: "Tax exemption for sales to Indigenous peoples under the Indian Act",
@@ -36,55 +42,91 @@ function Taxes() {
     },
   ]
 
+  const [ showTaxFormModal, setShowTaxFormModal ] = useState(false)
+  const [ selectedTax, setSelectedTax ] = useState<TaxProps | null>(null)
+
   return (
-    <div>
-      <div className="flex flex-col gap-2">
-        {taxes.map((tax) => (
-          <div key={tax.id} className="flex justify-between items-center gap-3 p-2 border rounded-lg">
+    <>
+      <div>
+        <div>
+          <FlexBox justifyContent="between" alignItems="center" className="mb-3" >
             <div>
-              <div className="flex flex-col" >
-                <FlexBox gap={3}>
-                  <div>{tax.name} ({tax.abbreviation})</div>
-                  <strong>{tax.rate}%</strong>
-                  {tax.default && <Tag variant="outlined" color="green">Default</Tag>}
-                </FlexBox>
-                <div className="text-ghost line-clamp-1">
-                  {tax.description}
+            </div>
+            <div>
+            <Button
+              variant="solid"
+              color="primary"
+              icon={<HugeiconsIcon icon={AddIcon} size={20} />}
+              onClick={() => {
+                setSelectedTax(null)
+                setShowTaxFormModal(true)
+              }}
+            >
+              Add Tax
+            </Button>
+            </div>
+          </FlexBox>
+        </div>
+        <div className="flex flex-col gap-2">
+          {taxes.map((tax) => (
+            <div key={tax.id} className="flex justify-between items-center gap-3 p-2 border rounded-lg">
+              <div>
+                <div className="flex flex-col" >
+                  <FlexBox gap={3}>
+                    <div>{tax.name} ({tax.abbreviation})</div>
+                    <strong>{tax.rate}%</strong>
+                    {tax.default && <Tag variant="outlined" color="green">Default</Tag>}
+                  </FlexBox>
+                  <div className="text-ghost line-clamp-1">
+                    {tax.description}
+                  </div>
                 </div>
               </div>
-            </div>
-            <FlexBox gap={1} >
-              {!tax.default && (
-                <Tooltip title="Set as Default">
+              <FlexBox gap={1} >
+                {!tax.default && (
+                  <Tooltip title="Set as Default">
+                    <Button
+                      variant="text"
+                      shape="circle"
+                      color="purple"
+                      icon={<HugeiconsIcon icon={StarIcon} size={20} />}
+                    />
+                  </Tooltip>
+                )}
+                <Tooltip title="Edit">
                   <Button
                     variant="text"
                     shape="circle"
-                    color="purple"
-                    icon={<HugeiconsIcon icon={StarIcon} size={20} />}
+                    color="default"
+                    icon={<HugeiconsIcon icon={Edit03Icon} size={20} />}
+                    onClick={() => {
+                      setSelectedTax(tax)
+                      setShowTaxFormModal(true)
+                    }}
                   />
                 </Tooltip>
-              )}
-              <Tooltip title="Edit">
-                <Button
-                  variant="text"
-                  shape="circle"
-                  color="default"
-                  icon={<HugeiconsIcon icon={Edit03Icon} size={20} />}
-                />
-              </Tooltip>
-              <Tooltip title="Delete">
-                <Button
-                  variant="text"
-                  shape="circle"
-                  color="danger"
-                  icon={<HugeiconsIcon icon={Delete02Icon} size={20} />}
-                />
-              </Tooltip>
-            </FlexBox>
-          </div>
-        ))}
+                <Tooltip title="Delete">
+                  <Button
+                    variant="text"
+                    shape="circle"
+                    color="danger"
+                    icon={<HugeiconsIcon icon={Delete02Icon} size={20} />}
+                  />
+                </Tooltip>
+              </FlexBox>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+
+      {/* Components */}
+
+      <TaxFormModal
+        show={showTaxFormModal}
+        selectedTax={selectedTax}
+        onClose={() => setShowTaxFormModal(false)}
+      />
+    </>
   )
 }
 
