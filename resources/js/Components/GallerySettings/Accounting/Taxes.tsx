0f6a6e05068c888e5ef358aa334942d669cde75c
Saeid Khajevand <saeid.kh7@gmail.com>
2026-03-11 00:51:58 -0400

    add optimistic UI for default tax selection in Taxes component

--- resources/js/Components/GallerySettings/Accounting/Taxes.tsx
+++ resources/js/Components/GallerySettings/Accounting/Taxes.tsx
@@ -13,6 +13,7 @@ function Taxes() {
 
   const [ showTaxFormModal, setShowTaxFormModal ] = useState(false)
   const [ selectedTax, setSelectedTax ] = useState<TaxProps | null>(null)
+  const [ optimisticDefaultTaxId, setOptimisticDefaultTaxId ] = useState<number | null>(null)
 
   const taxesQuery = useQuery({
     queryKey: ['taxes'],
@@ -33,11 +34,16 @@ function Taxes() {
 
   const setDefaultTaxMutation = useMutation({
     mutationFn: (taxId: number) => axios.post(route('taxes.set-default', { tax: taxId })),
-    onSuccess: () => {
-      taxesQuery.refetch()
+    onMutate: (taxId: number) => {
+      setOptimisticDefaultTaxId(taxId)
+    },
+    onSuccess: async () => {
+      await taxesQuery.refetch()
+      setOptimisticDefaultTaxId(null)
     },
     onError: (error: any) => {
       message.error(error.response?.data?.message || 'Failed to update default tax')
+      setOptimisticDefaultTaxId(null)
     }
   })
 
@@ -69,56 +75,59 @@ function Taxes() {
         </div>
         <div className="flex flex-col gap-2">
           {taxesQuery.isLoading && <LoadingSpinner size="large" />}
-          {taxesQuery.data?.map((tax: TaxProps) => (
-            <div key={tax.id} className="flex justify-between items-center gap-3 p-2 border rounded-lg">
-              <div>
-                <div className="flex flex-col" >
-                  <FlexBox gap={3}>
-                    <div>{tax.name} ({tax.abbreviation})</div>
-                    <strong>{Intl.NumberFormat().format(tax.rate)}%</strong>
-                    {tax.default && <Tag variant="outlined" color="green">Default</Tag>}
-                  </FlexBox>
-                  <div className="text-ghost line-clamp-1">
-                    {tax.description}
+          {taxesQuery.data?.map((tax: TaxProps) => {
+            const isDefault = optimisticDefaultTaxId ? tax.id === optimisticDefaultTaxId : tax.default
+            return (
+              <div key={tax.id} className="flex justify-between items-center gap-3 p-2 border rounded-lg">
+                <div>
+                  <div className="flex flex-col" >
+                    <FlexBox gap={3}>
+                      <div>{tax.name} ({tax.abbreviation})</div>
+                      <strong>{Intl.NumberFormat().format(tax.rate)}%</strong>
+                      {isDefault && <Tag variant="outlined" color="green">Default</Tag>}
+                    </FlexBox>
+                    <div className="text-ghost line-clamp-1">
+                      {tax.description}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <FlexBox gap={1} >
-                {!tax.default && (
-                  <Tooltip title="Set as Default">
+                <FlexBox gap={1} >
+                  {!isDefault && (
+                    <Tooltip title="Set as Default">
+                      <Button
+                        variant="text"
+                        shape="circle"
+                        color="purple"
+                        icon={<HugeiconsIcon icon={StarIcon} size={20} />}
+                        onClick={() => setDefaultTaxMutation.mutate(tax.id)}
+                      />
+                    </Tooltip>
+                  )}
+                  <Tooltip title="Edit">
                     <Button
                       variant="text"
                       shape="circle"
-                      color="purple"
-                      icon={<HugeiconsIcon icon={StarIcon} size={20} />}
-                      onClick={() => setDefaultTaxMutation.mutate(tax.id)}
+                      color="default"
+                      icon={<HugeiconsIcon icon={Edit03Icon} size={20} />}
+                      onClick={() => {
+                        setSelectedTax(tax)
+                        setShowTaxFormModal(true)
+                      }}
                     />
                   </Tooltip>
-                )}
-                <Tooltip title="Edit">
-                  <Button
-                    variant="text"
-                    shape="circle"
-                    color="default"
-                    icon={<HugeiconsIcon icon={Edit03Icon} size={20} />}
-                    onClick={() => {
-                      setSelectedTax(tax)
-                      setShowTaxFormModal(true)
-                    }}
-                  />
-                </Tooltip>
-                <Tooltip title="Delete">
-                  <Button
-                    variant="text"
-                    shape="circle"
-                    color="danger"
-                    icon={<HugeiconsIcon icon={Delete02Icon} size={20} />}
-                    onClick={() => deleteTaxMutation.mutate(tax.id)}
-                  />
-                </Tooltip>
-              </FlexBox>
-            </div>
-          ))}
+                  <Tooltip title="Delete">
+                    <Button
+                      variant="text"
+                      shape="circle"
+                      color="danger"
+                      icon={<HugeiconsIcon icon={Delete02Icon} size={20} />}
+                      onClick={() => deleteTaxMutation.mutate(tax.id)}
+                    />
+                  </Tooltip>
+                </FlexBox>
+              </div>
+            )
+          })}
           {taxesQuery.isFetched && taxesQuery.data?.length === 0 && (<Empty description="No taxes created yet" image={Empty.PRESENTED_IMAGE_SIMPLE} />)}
         </div>
       </div>
