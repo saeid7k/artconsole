46c0151ef59cebda5a0f4b3bfa061827499d1899
Saeid Khajevand <saeid.kh7@gmail.com>
2026-03-12 19:38:56 -0400

    add showAddress prop to ContactWidget

--- resources/js/Components/Contacts/ContactWidget.tsx
+++ resources/js/Components/Contacts/ContactWidget.tsx
@@ -7,23 +7,36 @@ import ContactAvatar from './ContactAvatar';
 type Props = {
   contact: ContactProps;
   title?: string;
-  viewButton?: boolean;
+  showAddress?: boolean;
+  showViewButton?: boolean;
   unsetFunction?: () => void;
   className?: string;
 }
 
-function ContactWidget({ contact, title = 'Contact', viewButton = true, unsetFunction, className }: Props) {
+function ContactWidget({ contact, title = 'Contact', showAddress = false, showViewButton = true, unsetFunction, className }: Props) {
   return (
-    <div className={`flex gap-1 ${className}`}>
-      <div className="flex items-center gap-1 border border-dashed border-soft rounded py-1 px-2 w-max h-max">
-        <ContactAvatar contact={contact} />
-        <div className="flex flex-col leading-tight">
-          <label>{title}</label>
-          <div>{contact?.full_name}</div>
+    <div className={`flex gap-1 max-w-[300px] ${className}`}>
+      <div className="border border-dashed border-soft rounded py-1 px-2 w-max h-max">
+        <div className="flex items-center gap-2">
+          <ContactAvatar contact={contact} />
+          <div className="flex flex-col leading-tight">
+            <label>{title}</label>
+            <div>{contact?.full_name}</div>
+          </div>
         </div>
+        {showAddress && contact?.formatted_address && (
+          <div
+            className='mt-2 pt-1'
+            style={{
+              borderTop: '1px dashed var(--border-soft)',
+            }}
+          >
+            {contact?.formatted_address}
+          </div>
+        )}
       </div>
       <div className="flex flex-col">
-        {viewButton && (
+        {showViewButton && (
           <Tooltip title="View in new tab" placement="right">
             <Button
               size='small'
