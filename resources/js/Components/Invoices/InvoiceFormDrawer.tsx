b5ae7f217dcc18223947c94c0f2ffff1d5b18996
Saeid Khajevand <saeid.kh7@gmail.com>
2026-03-12 19:09:55 -0400

    add basic fields and nextInvoiceNumberQuery to invoice form

--- resources/js/Components/Invoices/InvoiceFormDrawer.tsx
+++ resources/js/Components/Invoices/InvoiceFormDrawer.tsx
@@ -1,9 +1,13 @@
 import { useWindow } from "@/hooks/useWindow";
 import { InvoiceProps } from "@/types/invoice";
 import { router } from "@inertiajs/react";
-import { Button, Drawer, Form, message, Select } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import { Button, DatePicker, Drawer, Form, Input, message, Select } from "antd";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ContactWidget from "../Contacts/ContactWidget";
+import { AnimatePresence, motion } from "framer-motion";
+import AnimatedContainer from "../AnimatedContainer";
 
 type Props = {
   show: boolean;
@@ -19,6 +23,25 @@ function InvoiceFormDrawer({ show, onClose, selectedInvoice = null }: Props) {
   const [saving, setSaving] = useState(false);
 
   // Select Customer
+
+  const contactsQuery = useQuery({
+    queryKey: ['contacts'],
+    queryFn: () => axios.get(route('contacts.all')).then(res => res.data),
+  });
+
+  // Get next invoice number
+
+  const nextInvoiceNumberQuery = useQuery({
+    queryKey: ['next-invoice-number'],
+    queryFn: () => axios.get(route('invoices.next-number')).then(res => res.data.next_invoice_number),
+    enabled: show && !selectedInvoice,
+  })
+
+  useEffect(() => {
+    if (nextInvoiceNumberQuery.data && !selectedInvoice) {
+      form.setFieldValue('number', nextInvoiceNumberQuery.data);
+    }
+  }, [nextInvoiceNumberQuery.data])
 
   const handleClose = () => {
     onClose();
@@ -67,24 +90,77 @@ function InvoiceFormDrawer({ show, onClose, selectedInvoice = null }: Props) {
     >
       <Form
         form={form}
-        layout="vertical"
-        initialValues={selectedInvoice ? selectedInvoice : {}}
+        layout="horizontal"
+        initialValues={selectedInvoice ? selectedInvoice : {
+          contact_id: null,
+          number: '',
+          date: null,
+          due_date: null,
+        }}
         onFinish={handleSubmit}
       >
-        <div>
-          <Form.Item
-            name="contact_id"
-            label="Select Customer"
-            className="w-full sm:w-1/2"
-          >
-            <Select
-              options={[]}
-              placeholder="Select Contact"
-              showSearch={{ optionFilterProp: ['label', 'value'] }}
-              // onChange={(value: string) => onChangeArtist(value)}
-              // onDeselect={() => clearArtist()}
-            />
-          </Form.Item>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div>
+            <Form.Item
+              name="contact_id"
+              label="Select Customer"
+              className="w-full sm:max-w-[300px]"
+              hidden={formWatch.contact_id}
+            >
+              <Select
+                options={contactsQuery.data?.map((contact: any) => ({ label: contact.full_name, value: contact.id }))}
+                placeholder="Select Customer"
+                showSearch={{ optionFilterProp: ['label', 'value'] }}
+                loading={contactsQuery.isLoading}
+              />
+            </Form.Item>
+            <AnimatedContainer
+              condition={!!formWatch.contact_id}
+              type="fadeRight"
+            >
+              <ContactWidget
+                title="Customer"
+                contact={contactsQuery.data?.find((c: any) => c.id === formWatch.contact_id)}
+                unsetFunction={() => form.setFieldValue('contact_id', null)}
+              />
+            </AnimatedContainer>
+          </div>
+          <div className="w-full sm:max-w-[300px] flex flex-col items-end justify-self-end">
+            <Form.Item
+              name="number"
+              label="Number"
+              className="w-full"
+              labelCol={{ span: 8 }}
+              rules={[
+                { required: true, message: 'Please enter the invoice number' },
+                { max: 100, message: 'Invoice number cannot exceed 100 characters' },
+              ]}
+            >
+              <Input
+                placeholder="Invoice Number"
+                onChange={(e) => {
+                  const value = e.target.value;
+                  form.setFieldValue('number', value.replace(/\D/g, ''));
+                }}
+              />
+            </Form.Item>
+            <Form.Item
+              name="date"
+              label="Invoice Date"
+              className="w-full"
+              labelCol={{ span: 8 }}
+            >
+              <DatePicker className="w-full" />
+            </Form.Item>
+            <Form.Item
+              name="due_date"
+              label="Due Date"
+              className="w-full"
+              labelCol={{ span: 8 }}
+            >
+              <DatePicker className="w-full" />
+            </Form.Item>
+          </div>
         </div>
       </Form>
     </Drawer>
