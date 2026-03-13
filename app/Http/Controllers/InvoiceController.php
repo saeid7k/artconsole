4b6e891ee3977cc27ad4777de950bd2a1cceb7c5
Saeid Khajevand <saeid.kh7@gmail.com>
2026-03-13 13:04:06 -0400

    add store method to invoice controller

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\InvoiceStatus;
 use App\Models\Invoice;
 use Illuminate\Http\Request;
 
@@ -17,5 +18,28 @@ class InvoiceController extends Controller
     return response()->json([
       'next_invoice_number' => Invoice::nextInvoiceNumber(),
     ]);
+  }
+
+  public function store(Request $request)
+  {
+    $validated = $request->validate([
+      'contact_id' => 'required|exists:contacts,id',
+      'number' => 'required|string|max:100',
+      'date' => 'required|date',
+      'due_date' => 'sometimes|nullable|date|after_or_equal:date',
+    ]);
+
+    $suer = auth()->user();
+    $gallery = $suer->currentGallery();
+
+    $invoice = $gallery->invoices()->create($validated + [
+      'user_id' => $suer->id,
+      'status' => InvoiceStatus::default()->value,
+    ]);
+
+    return response()->json([
+      'message' => 'Invoice created successfully',
+      'invoice' => $invoice
+    ], 201);
   }
 }
