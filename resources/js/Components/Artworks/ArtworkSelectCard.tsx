89bf039bf30fa688898e2858bb8d68ea4030f399
Saeid Khajevand <saeid.kh7@gmail.com>
2026-03-13 23:17:27 -0400

    refactor ArtworkSelectCard to use isSelected prop and remove checkedItems and onClick

--- resources/js/Components/Artworks/ArtworkSelectCard.tsx
+++ resources/js/Components/Artworks/ArtworkSelectCard.tsx
@@ -6,19 +6,18 @@ import { HugeiconsIcon } from "@hugeicons/react";
 
 type Props = {
   artwork: ArtworkProps;
-  checkedItems?: ArtworkProps[];
-  onClick?: (artwork: ArtworkProps) => void;
+  isSelected?: boolean;
   size?: 'small' | 'default';
-}
+} & React.HTMLAttributes<HTMLDivElement>
 
-function ArtworkSelectCard({ artwork, checkedItems, onClick, size = 'default' }: Props) {
+function ArtworkSelectCard({ artwork, isSelected, size = 'default', ...props }: Props) {
   return (
     <div key={artwork.id}
       className={twMerge(
         "relative flex border hover:bg-primary-light cursor-pointer transition-all",
-        checkedItems && checkedItems.find((c) => c.id === artwork.id) && "bg-primary-light !border-primary"
+        isSelected && "bg-primary-light !border-primary"
       )}
-      onClick={onClick ? () => onClick(artwork) : undefined}
+      {...props}
     >
       {artwork.main_image_thumb_url && (
         <img
@@ -47,7 +46,7 @@ function ArtworkSelectCard({ artwork, checkedItems, onClick, size = 'default' }:
         icon={CheckmarkCircleIcon}
         className={twMerge(
           "absolute right-1 top-1 hidden",
-          checkedItems && checkedItems.find((c: any) => c.id === artwork.id) && "block text-primary"
+          isSelected && "block text-primary"
         )}
       />
     </div>
