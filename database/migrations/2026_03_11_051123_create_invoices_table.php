73d4cd3293bb7ae1e8ebaae24c196e035c18e6ea
Saeid Khajevand <saeid.kh7@gmail.com>
2026-03-16 00:14:50 -0400

    add shipping and discount fields to Invoice model and migration

--- database/migrations/2026_03_11_051123_create_invoices_table.php
+++ database/migrations/2026_03_11_051123_create_invoices_table.php
@@ -22,13 +22,19 @@ return new class extends Migration
       $table->date('date');
       $table->date('due_date')->nullable();
 
+      $table->json('shipping')->nullable();
+
       $table->foreignId('tax_id')->nullable()->constrained()->nullOnDelete();
       $table->string('tax_name', 255)->nullable();
       $table->decimal('tax_rate', 5, 2)->nullable();
 
-      $table->json('shipping')->nullable();
-
       $table->decimal('subtotal', 12, 2)->default(0);
+      $table->json('available_extra_costs')->nullable();
+      $table->decimal('shipping_cost', 12, 2)->default(0);
+      $table->boolean('shipping_taxable')->default(true);
+      $table->string('discount_type', 50)->nullable();
+      $table->decimal('discount_rate', 12, 2)->nullable();
+      $table->decimal('discount_amount', 12, 2)->default(0);
       $table->decimal('tax_amount', 12, 2)->default(0);
       $table->decimal('total', 12, 2)->default(0);
 
