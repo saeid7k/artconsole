a4f7e291df4651c2faa011968bc27b01fdfa6b92
Saeid Khajevand <saeid.kh7@gmail.com>
2026-03-18 11:34:56 -0400

    refactor AccountPreferencesTab to use useSaveChip for saving status management and remove unused code

--- resources/js/Components/Account/AccountPreferencesTab.tsx
+++ resources/js/Components/Account/AccountPreferencesTab.tsx
@@ -1,19 +1,16 @@
-import colors from "@/Themes/theme";
+import useSaveChip from "@/hooks/useSaveChip";
 import { AuthProps } from "@/types/auth";
 import { timeZoneOptions } from "@/utils/dateTimeHelper";
 import { usePage } from "@inertiajs/react";
 import { useMutation } from "@tanstack/react-query";
-import { message, Select, Tag } from "antd";
+import { message, Select } from "antd";
 import axios from "axios";
-import { AnimatePresence, motion } from "framer-motion";
-import { useEffect, useState } from "react";
 import FlexBox from "../Containers/FlexBox";
 
 function AccountPreferencesTab() {
 
   const { user } = usePage().props.auth as AuthProps
-
-  const [savingStatus, setSavingStatus] = useState<null | "pending" | "saved">(null);
+  const { setSavingStatus, saveChipNode } = useSaveChip();
 
   const setMetaMutation = useMutation({
     mutationFn: ({ key, value }: { key: string; value: string }) => {
@@ -24,43 +21,18 @@ function AccountPreferencesTab() {
     },
     onError: (err: any) => {
       message.error(err.response.data.message || "An error occurred");
+      setSavingStatus("failed");
     }
   });
 
   function handleChange(key: string, value: string) {
-    setSavingStatus("pending");
+    setSavingStatus("saving");
     setMetaMutation.mutate({ key, value });
   }
 
-  useEffect(() => {
-    // clear saved status after 3 seconds
-    if (savingStatus === "saved") {
-      const timer = setTimeout(() => {
-        setSavingStatus(null);
-      }, 3000);
-      return () => clearTimeout(timer);
-    }
-  }, [savingStatus])
-
   return (
     <div>
-      <AnimatePresence>
-        {savingStatus && (
-          <motion.div
-            initial={{ opacity: 0, x: -10 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: 10 }}
-            transition={{ duration: 0.3 }}
-            style={{ position: "absolute", top: 10, right: 10 }}
-          >
-            <Tag
-              color={savingStatus === 'saved' ? colors.green[600] : colors.blue[500]}
-            >
-              {savingStatus === 'saved' ? 'Saved' : savingStatus === 'pending' ? 'Saving...' : 'Idle'}
-            </Tag>
-          </motion.div>
-        )}
-      </AnimatePresence>
+      {saveChipNode}
       <div className="label">Timezone</div>
       <FlexBox>
         <Select
