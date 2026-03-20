c7a4bc5baab91d601a80d644e7ef5f8b6dd931c4
Saeid Khajevand <saeid.kh7@gmail.com>
2026-03-20 14:03:42 -0400

    add get method to InvoiceController for retrieving a single invoice

--- routes/web.php
+++ routes/web.php
@@ -139,6 +139,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', [App\Http\Controllers\InvoiceController::class, 'index'])->name('index');
     Route::post('/store', [App\Http\Controllers\InvoiceController::class, 'store'])->name('store');
     Route::get('/next-number', [App\Http\Controllers\InvoiceController::class, 'nextInvoiceNumber'])->name('next-number');
+    Route::get('/{invoice}/get', [App\Http\Controllers\InvoiceController::class, 'get'])->name('get');
   });
 });
 
