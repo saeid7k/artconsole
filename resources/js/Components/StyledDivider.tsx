f215866616ba9bba714edfdc4d16039a2ef097d7
Saeid Khajevand <saeid.kh7@gmail.com>
2026-03-27 15:41:58 -0400

    enhance StyledDivider component to support dashed lines and customizable line colors

--- resources/js/Components/StyledDivider.tsx
+++ resources/js/Components/StyledDivider.tsx
@@ -2,20 +2,28 @@ import { Divider } from "antd";
 
 type Props = {
   variant?: 'default'|'light';
+  dashed?: boolean;
+  lineColor?: 'soft' | 'light';
   rootClassName?: string;
   children?: React.ReactNode;
 }
 
-function StyledDivider({ variant = 'default', rootClassName = '', children }: Props) {
+function StyledDivider({ variant = 'default', dashed = false, lineColor = 'light', rootClassName = '', children }: Props) {
   const variantClasses = {
     default: '',
     light: 'text-muted font-light tracking-wide',
   }
 
+  const lineColorClasses = {
+    soft: 'border-soft',
+    light: '',
+  }
+
   return (
     <Divider
       plain
-      className={rootClassName}
+      className={rootClassName + ' ' + lineColorClasses[lineColor]}
+      dashed={dashed}
     >
       <div className={variantClasses[variant]}>
         {children}
