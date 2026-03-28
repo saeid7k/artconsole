0ab842a6a9c83944a3f372cab0c377574804d541
Saeid Khajevand <saeid.kh7@gmail.com>
2026-03-28 11:12:26 -0400

    add Amount Due and Due Remaining Days to Invoice table

--- resources/js/Components/Invoices/InvoiceTable.tsx
+++ resources/js/Components/Invoices/InvoiceTable.tsx
@@ -12,10 +12,11 @@ import StyledDate from "../StyledDate";
 import InvoiceFormDrawer from "./InvoiceFormDrawer";
 import InvoiceStatusTag from "./InvoiceStatusTag";
 import InvoicesActions from "./InvoicesActions";
+import { PENDING_INVOICE_STATUSES } from "@/constants/invoiceStatuses";
+import { twMerge } from "tailwind-merge";
 
 function InvoiceTable({ invoices }: any) {
 
-  const gallery = usePage<UsePageProps>()?.props?.current_gallery
   const { breakpoint } = useWindow()
 
   const [showInvoiceFormDrawer, setShowInvoiceFormDrawer] = useState(false)
@@ -73,20 +74,51 @@ function InvoiceTable({ invoices }: any) {
       render: (record: ContactProps) => <div>{record.full_name}</div>
     },
     {
-      title: 'Amount',
+      title: 'Total',
       dataIndex: 'total',
       key: 'total',
       sorter: true,
       sortDirections: ['ascend', 'descend'],
       showSorterTooltip: false,
-      render: (text: string) => <StyledCurrency value={text} />,
-      align: "right"
+      render: (text: string) => <StyledCurrency value={text} className="font-bold" />,
+      align: "right",
+      width: 150,
+    },
+    {
+      title: 'Amount Due',
+      dataIndex: 'amount_due',
+      key: 'amount_due',
+      sorter: true,
+      sortDirections: ['ascend', 'descend'],
+      showSorterTooltip: false,
+      render: (text: string) => Number(text) == 0 ? '-' : <StyledCurrency value={text} />,
+      align: "right",
+      width: 150,
     },
     {
       title: 'Status',
       dataIndex: 'status',
       key: 'status',
-      render: (text: string, record: any) => <InvoiceStatusTag invoice={record} />,
+      render: (text: string, record: any) => (
+        <div className="flex flex-col">
+          <InvoiceStatusTag invoice={record} />
+          {PENDING_INVOICE_STATUSES.includes(record.status) && (
+            <div
+              className={twMerge(
+                "text-xs",
+                record.due_remaining_days < 0 ? "text-red-500" : "text-muted"
+              )}
+            >
+              {record.due_remaining_days === 0 && 'Due today'}
+              {record.due_remaining_days !== 0 && (
+                <>
+                  {Math.abs(record.due_remaining_days)} days {record.due_remaining_days < 0 ? 'overdue' : 'to due'}
+                </>
+              )}
+            </div>
+          )}
+        </div>
+      ),
       width: 180,
     },
     {
@@ -94,6 +126,7 @@ function InvoiceTable({ invoices }: any) {
       key: 'actions',
       render: (_: any, record: any) => <InvoicesActions invoice={record} />,
       fixed: ['xs', 'sm'].includes(breakpoint) ? undefined : 'right',
+      width: 150,
     }
   ]
 
