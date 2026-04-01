01222fe92bbafa958b98e9bb7f23c5a5a1ec2ab1
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-01 10:20:47 -0400

    add formatPhoneNumber method to FormatHelper

--- app/Helpers/FormatHelper.php
+++ app/Helpers/FormatHelper.php
@@ -53,4 +53,19 @@ class FormatHelper
 
     return implode(', ', $array);
   }
+
+  public static function formatPhoneNumber($phone, $countryCode = '+1'): string
+  {
+    if (!$phone) {
+      return '-';
+    }
+
+    $digits = preg_replace('/\D+/', '', $phone);
+
+    if (strlen($digits) === 10 && $countryCode === '+1') {
+      return '('.substr($digits, 0, 3).') '.substr($digits, 3, 3).'-'.substr($digits, 6);
+    }
+
+    return $phone;
+  }
 }
