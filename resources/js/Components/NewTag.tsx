9be423cd4965e4fcfd03b3e3ff7c73d5361655c1
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-04 00:22:55 -0400

    update color handling to use oklchToHex in NewTag and useSaveChip

--- resources/js/Components/NewTag.tsx
+++ resources/js/Components/NewTag.tsx
@@ -2,12 +2,13 @@ import colors from "@/Themes/theme";
 import { Tag } from "antd";
 import dayjs from "dayjs";
 import CONFIGS from "@/constants/configs.json";
+import { oklchToHex } from "@/utils/colorHelper";
 
 function NewTag({ dateRef }: { dateRef: string }) {
   return (
     <>
       {dayjs(dateRef).isAfter(dayjs().subtract(CONFIGS.new_tag_timeout, 'minutes')) && (
-        <Tag color={colors.sky[500]} >New</Tag>
+        <Tag color={oklchToHex(colors.sky[500])} >New</Tag>
       )}
     </>
   )
