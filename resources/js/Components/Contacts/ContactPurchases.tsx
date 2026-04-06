357b0893de941dd8b03e2ee1cd0df4f07fcc99ff
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-05 22:36:14 -0400

    update ui of ContactPurchases component

--- resources/js/Components/Contacts/ContactPurchases.tsx
+++ resources/js/Components/Contacts/ContactPurchases.tsx
@@ -10,6 +10,7 @@ import StyledDate from "../StyledDate";
 import InvoiceStatusTag from "../Invoices/InvoiceStatusTag";
 import ArtworkTitleStack from "../Artworks/ArtworkTitleStack";
 import { Link } from "@inertiajs/react";
+import StyledCurrency from "../StyledCurrency";
 
 type Props = {
   contact: ContactProps;
@@ -32,70 +33,74 @@ function ContactPurchases({ contact }: Props) {
   }
 
   return (
-    <div className="flex flex-col gap-3 mt-3 w-full">
-      {invoicesQuery.data?.map((invoice: InvoiceProps) => {
-        let artworks = invoice.artworks || [];
-        let customItems = invoice.items ? invoice.items.filter(item => item.type === 'custom') : [];
-        let extraItemsLength = (Math.max(artworks.length - 2, 0)) + customItems.length;
-        return (
-          /* Row */
-          <Card
-            size="small"
-            key={invoice.id}
-            className="w-full overflow-x-auto group hover:bg-primary/5 transition"
-          >
-            <div className="flex flex-col xl:flex-row gap-3 items-start xl:items-center">
+    <div className="h-[65vh] overflow-y-auto">
+      <div className="flex flex-col gap-3 mt-3 w-full">
+        {invoicesQuery.data?.map((invoice: InvoiceProps) => {
+          let artworks = invoice.artworks || [];
+          let customItems = invoice.items ? invoice.items.filter(item => item.type === 'custom') : [];
+          let extraItemsLength = (Math.max(artworks.length - 2, 0)) + customItems.length;
+          return (
+            /* Row */
+            <Card
+              size="small"
+              key={invoice.id}
+              className="w-full overflow-x-auto group hover:bg-primary/5 transition"
+            >
+              <div className="flex flex-col xl:flex-row gap-3 items-start xl:items-center">
 
-              {/* Left Column */}
-              <div className="flex flex-col xl:flex-row gap-3 xl:items-center grow" >
-                <FlexBox direction="col" alignItems="start" gap={0} >
-                  <div className="label !mb-0 ps-2">Invoice #</div>
-                  <InvoiceNumberStack invoice={invoice} />
-                </FlexBox>
-                <Divider vertical className="h-10 hidden xl:block" />
-
-                {/* Invoice Items */}
-                {invoice.items && invoice.items.length > 0 && (
-                  <FlexBox
-                    gap={3}
-                    // className="w-max"
-                    wrapping="wrap"
-                  >
-                    {artworks.slice(0, 2).map(artwork => (
-                      <Link href={route('artworks.show', artwork.id)} key={artwork.id}>
-                        <FlexBox
-                          className="border !border-gray-500/0 hover:!border-gray-500/30 p-1 rounded transition"
-                        >
-                          <img
-                            src={artwork.main_image_thumb_url ?? ''}
-                            alt={artwork.title}
-                            className="w-16 h-16 object-cover rounded"
-                          />
-                          <ArtworkTitleStack artwork={artwork} showYear={false} showSigned={false} showConsignment={false} disableLinks />
-                        </FlexBox>
-                      </Link>
-                    ))}
-                    {extraItemsLength > 0 && (
-                      <div
-                        className="border border-light rounded py-1 px-2 content-center text-center leading-tight h-16 text-soft"
-                      >
-                        <div>+{extraItemsLength}</div>
-                        <div>item{extraItemsLength > 1 ? 's' : ''}</div>
-                      </div>
-                    )}
+                {/* Left Column */}
+                <div className="flex flex-col xl:flex-row gap-3 xl:items-center grow" >
+                  <FlexBox direction="col" alignItems="start" gap={0} >
+                    <div className="label !mb-0 ps-2">Invoice #</div>
+                    <InvoiceNumberStack invoice={invoice} />
                   </FlexBox>
-                )}
-              </div>
+                  <Divider vertical className="h-10 hidden xl:block" />
 
-              {/* Right Column */}
-              <FlexBox gap={3} >
-                <InvoiceStatusTag invoice={invoice} />
-                <StyledDate value={invoice.created_at} showTime={false} />
-              </FlexBox>
-            </div>
-          </Card>
-        )
-      })}
+                  {/* Invoice Items */}
+                  {invoice.items && invoice.items.length > 0 && (
+                    <FlexBox
+                      gap={3}
+                      // className="w-max"
+                      wrapping="wrap"
+                    >
+                      {artworks.slice(0, 2).map(artwork => (
+                        <Link href={route('artworks.show', artwork.id)} key={artwork.id}>
+                          <FlexBox
+                            className="border !border-gray-500/0 hover:!border-gray-500/30 p-1 rounded transition"
+                          >
+                            <img
+                              src={artwork.main_image_thumb_url ?? ''}
+                              alt={artwork.title}
+                              className="w-16 h-16 object-cover rounded"
+                            />
+                            <ArtworkTitleStack artwork={artwork} showYear={false} showSigned={false} showConsignment={false} disableLinks />
+                          </FlexBox>
+                        </Link>
+                      ))}
+                      {extraItemsLength > 0 && (
+                        <div
+                          className="border border-light rounded py-1 px-2 content-center text-center leading-tight h-16 text-muted"
+                        >
+                          <div>+{extraItemsLength}</div>
+                          <div>extra</div>
+                          <div>item{extraItemsLength > 1 ? 's' : ''}</div>
+                        </div>
+                      )}
+                    </FlexBox>
+                  )}
+                </div>
+
+                {/* Right Column */}
+                <div className="flex items-center gap-3 flex-wrap md:flex-nowrap">
+                  <StyledCurrency value={invoice.total} />
+                  <InvoiceStatusTag invoice={invoice} />
+                  <StyledDate value={invoice.created_at} showTime={false} />
+                </div>
+              </div>
+            </Card>
+          )
+        })}
+      </div>
     </div>
   );
 }
