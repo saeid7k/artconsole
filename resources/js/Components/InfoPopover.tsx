4d755c5795ac1610694fe603d35fa7e560fa73dc
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-06 12:39:27 -0400

    update InfoPopover component to support React nodes and tooltip placement

--- resources/js/Components/InfoPopover.tsx
+++ resources/js/Components/InfoPopover.tsx
@@ -2,14 +2,17 @@ import colors from "@/Themes/theme";
 import { InformationCircleIcon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { Popover } from "antd";
+import { TooltipPlacement } from "antd/es/tooltip";
+import React from "react";
 
 type Props = {
   title?: string;
-  content: string;
+  content: string | React.ReactNode;
+  placement?: TooltipPlacement;
   condition?: boolean;
 }
 
-function InfoPopover({ title, content, condition = true }: Props) {
+function InfoPopover({ title, content, placement, condition = true }: Props) {
 
   if (!condition) return null;
 
@@ -17,6 +20,7 @@ function InfoPopover({ title, content, condition = true }: Props) {
     <Popover
       title={title}
       content={content}
+      placement={placement || undefined}
     >
       <HugeiconsIcon icon={InformationCircleIcon} size={16} color={colors.gray[500]} />
     </Popover>
