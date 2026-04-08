e0e84215d0772060112a6074ddf193c20d83b85a
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-08 00:28:33 -0400

    add ArtworkFinancialForm component for managing financial details of artworks

--- resources/js/Components/Artworks/ArtworkFinancial.tsx
+++ resources/js/Components/Artworks/ArtworkFinancial.tsx
@@ -3,6 +3,7 @@ import FlexBox from "../Containers/FlexBox";
 import InvoiceNumberStack from "../Invoices/InvoiceNumberStack";
 import StyledCurrency from "../StyledCurrency";
 import StyledDate from "../StyledDate";
+import ArtworkFinancialForm from "./ArtworkFinancialForm";
 
 function ArtworkFinancial({ artwork }: { artwork: ArtworkProps }) {
 
@@ -10,73 +11,84 @@ function ArtworkFinancial({ artwork }: { artwork: ArtworkProps }) {
   const profitAmount = salePrice && artwork.acquisition_price ? salePrice - artwork.acquisition_price : null
 
   return (
-    <div>
-      <table className="all-border group">
-        <tbody>
-          <tr>
-            <td>Acquisition</td>
-            <td>
-              {artwork.acquisition_price ? (
-                <StyledDate
-                  value={artwork.acquisition_date ?? null}
-                  showTime={false}
-                />
-              ) : (
-                <span className="text-ghost">No data</span>
-              )}
-            </td>
-            <td className="min-w-[80px] text-right">
-              {artwork.acquisition_price ? (
-                <StyledCurrency
-                  value={artwork.acquisition_price ?? null}
-                />
-              ) : (
-                <span className="text-ghost">-</span>
-              )}
-            </td>
-          </tr>
-          <tr>
-            <td>Sale</td>
-            <td>
-              {artwork.last_invoice ? (
-                <FlexBox gap={3}>
+    <div
+      className="grid grid-cols-1 xl:grid-cols-2 items-start gap-10 max-w-[1000px]"
+    >
+      <div
+        className="bg-light p-3 border !border-dashed rounded"
+      >
+        <ArtworkFinancialForm artwork={artwork} />
+      </div>
+      <div
+        className="w-full overflow-x-auto"
+      >
+        <table className="all-border group">
+          <tbody>
+            <tr>
+              <td>Acquisition</td>
+              <td>
+                {artwork.acquisition_price ? (
                   <StyledDate
-                    value={artwork.last_invoice?.date ?? null}
+                    value={artwork.acquisition_date ?? null}
                     showTime={false}
                   />
-                  <InvoiceNumberStack invoice={artwork.last_invoice} />
-                </FlexBox>
-              ) : (
-                <span className="text-ghost">No data</span>
-              )}
-            </td>
-            <td className="text-right">
-              {artwork.last_invoice ? (
-                <StyledCurrency
-                  value={salePrice}
-                />
-              ) : (
-                <span className="text-ghost">-</span>
-              )}
-            </td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <th colSpan={2} className="text-right" >{(profitAmount ?? 0) >= 0 ? "Profit" : "Loss"}</th>
-            <th className="text-right">
-              {profitAmount ? (
-                <StyledCurrency
-                  value={profitAmount}
-                  greenOnPositive
-                />
-              ) : (
-                <span className="text-ghost">-</span>
-              )}
-            </th>
-          </tr>
-        </tfoot>
-      </table>
+                ) : (
+                  <span className="text-ghost">No data</span>
+                )}
+              </td>
+              <td className="min-w-[80px] text-right">
+                {artwork.acquisition_price ? (
+                  <StyledCurrency
+                    value={artwork.acquisition_price ?? null}
+                  />
+                ) : (
+                  <span className="text-ghost">-</span>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>Sale</td>
+              <td>
+                {artwork.last_invoice ? (
+                  <FlexBox gap={3}>
+                    <StyledDate
+                      value={artwork.last_invoice?.date ?? null}
+                      showTime={false}
+                    />
+                    <InvoiceNumberStack invoice={artwork.last_invoice} />
+                  </FlexBox>
+                ) : (
+                  <span className="text-ghost">No data</span>
+                )}
+              </td>
+              <td className="text-right">
+                {artwork.last_invoice ? (
+                  <StyledCurrency
+                    value={salePrice}
+                  />
+                ) : (
+                  <span className="text-ghost">-</span>
+                )}
+              </td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th colSpan={2} className="text-right" >{(profitAmount ?? 0) >= 0 ? "Profit" : "Loss"}</th>
+              <th className="text-right">
+                {profitAmount ? (
+                  <StyledCurrency
+                    value={profitAmount}
+                    greenOnPositive
+                  />
+                ) : (
+                  <span className="text-ghost">-</span>
+                )}
+              </th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </div>
   );
 }
