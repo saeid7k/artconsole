b6925d675ce2f139fa7465531f8407b5ba5e9cfb
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-13 23:19:32 -0400

    create subscription plans constants for Free and Professional tiers

---
+++ resources/js/constants/subscriptionPlans.ts
@@ -0,0 +1,26 @@
+
+const PLANS = [
+  {
+    "name": "Free",
+    "title": "Free",
+    "features": [
+      {"title": "Single User", "icon": "check"},
+      {"title": "Unlimited Artworks", "icon": "check"},
+      {"title": "Unlimited Locations", "icon": "check"},
+      {"title": "ArtConsole Branding", "icon": "info"}
+    ]
+  },
+  {
+    "name": "Professional",
+    "title": "Pro",
+    "features": [
+      {"title": "Unlimited Users", "icon": "check"},
+      {"title": "Unlimited Artworks", "icon": "check"},
+      {"title": "Unlimited Locations", "icon": "check"},
+      {"title": "Remove ArtConsole Branding", "icon": "check"},
+      {"title": "Priority Support", "icon": "check"}
+    ]
+  }
+]
+
+export default PLANS;
