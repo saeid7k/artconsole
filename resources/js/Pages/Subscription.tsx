f9c88b9acb311ad0cbf18e0697278376748372f3
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-14 23:44:07 -0400

    add subscription functionality with checkout process and success/cancel messages

--- resources/js/Pages/Subscription.tsx
+++ resources/js/Pages/Subscription.tsx
@@ -1,11 +1,32 @@
 import PageTitle from "@/Components/PageTitle";
 import AppLayout from "@/Layouts/AppLayout";
+import { getQueryParam } from "@/utils/urlHelper";
+import { message } from "antd";
+import { useEffect, useRef } from "react";
 
 function Subscription() {
+
+  const isInitialRender = useRef(true);
+  const [messageApi, contextHolder] = message.useMessage();
+
+  useEffect(() => {
+    if (isInitialRender.current) {
+      if (getQueryParam('checkout') === 'success') {
+        messageApi.success('Checkout successful!');
+      } else if (getQueryParam('checkout') === 'canceled') {
+        messageApi.error('Checkout canceled.');
+      }
+      isInitialRender.current = false;
+    }
+  }, [])
+
   return (
-    <PageTitle
-      title="Subscription"
-    />
+    <div>
+      {contextHolder}
+      <PageTitle
+        title="Subscription"
+      />
+    </div>
   );
 }
 
