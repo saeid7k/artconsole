39108ee9543baa39897ef429bdcf16423205fdba
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-18 12:19:05 -0400

    update DataCol component to enhance title styling

--- resources/js/Components/Containers/DataCol.tsx
+++ resources/js/Components/Containers/DataCol.tsx
@@ -10,7 +10,7 @@ type Props = {
 function DataCol({ title, gap = 1, className = '', children = null }: Props) {
   return (
     <div className={`flex flex-col gap-${gap} ${className}`}>
-      {title && <h4 className="m-0">{title}</h4>}
+      {title && <h4 className="font-semibold !m-0">{title}</h4>}
       {children}
     </div>
   )
