1406c48d50322f60347a9d8cba79c3823586e87c
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-21 22:57:46 -0400

    add ProBadge component for subscription upgrade prompt

---
+++ resources/js/Components/Subscription/ProBadge.tsx
@@ -0,0 +1,44 @@
+import { useApp } from "@/contexts/AppContext";
+import { UsePageProps } from "@/types/usePage";
+import { Rocket01Icon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { usePage } from "@inertiajs/react";
+import { Button } from "antd";
+
+type Props = {
+  size?: 'small' | 'middle' | 'medium' | 'large';
+  onClick?: () => void;
+}
+
+function ProBadge({ size = 'small', onClick }: Props) {
+
+  const gallery = usePage<UsePageProps>().props?.current_gallery
+  const { setOpenUpgradeModal } = useApp()
+
+  const isPro = gallery?.is_subscribed
+
+  function handleClick() {
+    if (onClick) {
+      onClick();
+    }
+    setOpenUpgradeModal(true);
+  }
+
+  if (isPro) {
+    return null
+  }
+
+  return (
+    <Button
+      size={size}
+      variant="solid"
+      color="purple"
+      icon={<HugeiconsIcon icon={Rocket01Icon} size={16} />}
+      onClick={handleClick}
+    >
+      PRO
+    </Button>
+  )
+}
+
+export default ProBadge;
