8667a9fc5577dfb4bb7893ada035bd5c74592138
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-24 15:11:17 -0400

    refactor HtmlRenderer to sanitize HTML content and improve whitespace handling

--- resources/js/Components/HtmlRenderer.tsx
+++ resources/js/Components/HtmlRenderer.tsx
@@ -2,12 +2,15 @@ import parse from 'html-react-parser';
 import DOMPurify from 'dompurify';
 
 const HtmlRenderer = ({ htmlContent }: { htmlContent: string }) => {
-  const cleanHtmlString = DOMPurify.sanitize(htmlContent, {
+
+  const contentWithNormalSpaces = htmlContent.replace(/&nbsp;/g, ' ');
+
+  const cleanHtmlString = DOMPurify.sanitize(contentWithNormalSpaces, {
     USE_PROFILES: { html: true },
   });
 
   return (
-    <div className="prose">
+    <div className="whitespace-pre-wrap break-words">
       {parse(cleanHtmlString)}
     </div>
   );
