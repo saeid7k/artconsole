743a8e0481b86686ab497b48565b6cc038f02cbd
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-24 23:57:30 -0400

    set report date in creator's timezone

--- resources/views/reports/header.blade.php
+++ resources/views/reports/header.blade.php
@@ -46,7 +46,7 @@
       <h1>{{ $report->type_title }}</h1>
     </div>
     <div style="text-align: right; padding-top: 0.25in; line-height: 1.5;" >
-      <div>{{ $report->created_at->format('F j, Y') }}</div>
+      <div>{{ $report->created_at->setTimezone($report->creator->timezone)->format('F j, Y') }}</div>
       <div>Total pieces: {{ count($report->artworks) }}</div>
     </div>
   </div>
