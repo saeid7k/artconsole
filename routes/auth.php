41dd71582b02522a7e4d78a175f127bdabbf5987
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-26 09:20:22 -0400

    Add demo mode and registration button in Sidebar component

--- routes/auth.php
+++ routes/auth.php
@@ -13,8 +13,9 @@ use App\Http\Controllers\GoogleAuthController;
 use App\Http\Middleware\Admin;
 use Illuminate\Support\Facades\Route;
 
+Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+
 Route::middleware('guest')->group(function () {
-  Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
   Route::post('register', [RegisteredUserController::class, 'store']);
   Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
   Route::post('login', [AuthenticatedSessionController::class, 'store']);
