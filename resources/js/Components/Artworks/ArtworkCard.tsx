383ada885fcdc9832a4777b18850eef6ea6d6e13
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-26 19:31:13 -0400

    update ArtworkCard style

--- resources/js/Components/Artworks/ArtworkCard.tsx
+++ resources/js/Components/Artworks/ArtworkCard.tsx
@@ -1,13 +1,12 @@
 import { ArtworkProps } from "@/types/artwork";
-import { formatCurrency } from "@/utils/formatHelper";
 import { router } from "@inertiajs/react";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 import imagePlaceholder from '~/resources/images/image-placeholder.svg';
 import FormattedDimensions from "../FormattedDimensions";
-import ArtworkTitleStack from "./ArtworkTitleStack";
-import ArtworkStatusTag from "./ArtworkStatusTag";
 import StyledCurrency from "../StyledCurrency";
+import ArtworkStatusTag from "./ArtworkStatusTag";
+import ArtworkTitleStack from "./ArtworkTitleStack";
 
 function ArtworkCard({ artwork }: { artwork: ArtworkProps }) {
 
@@ -22,7 +21,8 @@ function ArtworkCard({ artwork }: { artwork: ArtworkProps }) {
           <img
             src={artwork.main_image_url ?? imagePlaceholder}
             alt={artwork.title}
-            className={twMerge("relative block w-full cursor-pointer hover:scale-105 origin-bottom transition-transform duration-300",
+            className={twMerge(
+              "relative block w-full max-h-[300px] object-cover cursor-pointer hover:scale-105 origin-bottom transition-transform duration-300",
               artwork.main_image_url ? "" : "opacity-70"
             )}
             onLoad={() => setImageLoaded(true)}
@@ -30,8 +30,14 @@ function ArtworkCard({ artwork }: { artwork: ArtworkProps }) {
           />
           <FormattedDimensions
             dimensions={artwork?.dimensions || null}
-            className="absolute bottom-0 left-0 bg-white dark:bg-black !bg-opacity-60 px-1 text-xs"
+            className="absolute bottom-0 left-0 bg-white/50 dark:bg-black/50 !bg-opacity-60 px-1 text-xs"
           />
+          <div className="absolute top-0.5 right-1">
+            <ArtworkStatusTag
+              status={artwork.status}
+              fontSize="xs"
+            />
+          </div>
         </div>
       </div>
       <div className="flex flex-col gap-1 p-2 text-center">
@@ -40,13 +46,10 @@ function ArtworkCard({ artwork }: { artwork: ArtworkProps }) {
           rootClassName="text-center justify-center items-center"
           artistTooltipPlacement="right"
           showEdition={false}
+          showYear={false}
         />
+        <StyledCurrency value={artwork.price} />
         <div className="flex justify-center items-center gap-2">
-          <StyledCurrency value={artwork.price} />
-          <ArtworkStatusTag
-            status={artwork.status}
-            fontSize="xs"
-          />
         </div>
       </div>
     </div>
