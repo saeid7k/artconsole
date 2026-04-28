ea5412815b152a3b3c4cc401b063e9439c69f62d
Saeid Khajevand <saeid.kh7@gmail.com>
2026-04-27 23:21:46 -0400

    create MainMenu component for ai widgets

--- resources/js/Components/AI/AiDrawer.tsx
+++ resources/js/Components/AI/AiDrawer.tsx
@@ -1,23 +1,12 @@
 import { useApp } from "@/contexts/AppContext";
-import { Sofa01Icon } from "@hugeicons/core-free-icons";
-import { HugeiconsIcon } from "@hugeicons/react";
-import { Divider, Drawer } from "antd";
+import { Drawer } from "antd";
+import MainMenu from "./MainMenu";
+import { useState } from "react";
 
 function AiDrawer() {
 
   const { aiDrawerOpen, setAiDrawerOpen } = useApp()
-
-  const renderItemRow = (icon: React.ReactNode, label: string) => (
-    <div
-      className="flex items-center gap-1 border px-3 py-2 rounded-lg hover:bg-primary-500/5 transition duration-300 cursor-pointer"
-    >
-      {icon}
-      <div className="self-stretch">
-        <Divider orientation="vertical" className="h-[100%]" />
-      </div>
-      <div>{label}</div>
-    </div>
-  )
+  const [aiWidgetsStep, setAiWidgetStep] = useState('main-menu')
 
   return (
     <Drawer
@@ -27,7 +16,7 @@ function AiDrawer() {
       resizable
       title='AI Assistant'
     >
-      {renderItemRow(<HugeiconsIcon icon={Sofa01Icon} />, 'Create room mockup image')}
+      <MainMenu />
     </Drawer>
   )
 }
