da7f4a8a7bdf9d688f672dd130449fbfa465c702
Saeid Khajevand <saeid.kh7@gmail.com>
2026-05-03 23:39:50 -0400

    add recentSessions method and route to retrieve user's recent assistant conversations

--- routes/web.php
+++ routes/web.php
@@ -178,6 +178,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
   Route::prefix('ai')->name('ai.')->group(function () {
     Route::post('/get-resource', [App\Http\Controllers\AiController::class, 'getResource'])->name('get-resource');
+    Route::post('recent-sessions', [App\Http\Controllers\AiController::class, 'recentSessions'])->name('recent-sessions');
     Route::prefix('mockup')->name('mockup.')->group(function () {
       Route::post('/generate', [App\Http\Controllers\AiController::class, 'generateMockup'])->name('generate');
       Route::get('/{conversationId}/result', [App\Http\Controllers\AiController::class, 'getMockupResult'])->name('result');
