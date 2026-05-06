c60cc967d5fe4546a9a91ecfc24d1b9a6364cbb1
Saeid Khajevand <saeid.kh7@gmail.com>
2026-05-05 23:07:19 -0400

    add addToArtwork functionality for mockup responses

--- app/Http/Controllers/AgentMessageController.php
+++ app/Http/Controllers/AgentMessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\AgentConversationMessage;
+use App\Models\Artwork;
 use App\Models\Media;
 use Illuminate\Http\Request;
 
@@ -27,5 +28,38 @@ class AgentMessageController extends Controller
     }
 
     return response()->json($media);
+  }
+
+  public function addToArtwork(Request $request)
+  {
+    $messageId = $request->input('message_id');
+    $message = AgentConversationMessage::find($messageId);
+    if (!$message) {
+      return response()->json(['error' => 'Message not found'], 404);
+    }
+
+    $attachment = collect($message->attachments)->first();
+
+    if (!$attachment) {
+      return response()->json(['error' => 'Attachment not found'], 404);
+    }
+
+    $media = Media::find($attachment['media_id']);
+    if (!$media) {
+      return response()->json(['error' => 'Media not found'], 404);
+    }
+    $fileContent = file_get_contents($media->getPath());
+
+    $artwork = Artwork::find($message->artwork_id);
+    if (!$artwork) {
+      return response()->json(['error' => 'Artwork not found'], 404);
+    }
+
+    $artwork->addMediaFromString($fileContent)
+      ->usingFileName($media->file_name)
+      ->withCustomProperties($media->custom_properties)
+      ->toMediaCollection('artwork-images');
+
+    return response()->json(['success' => 'Media added to artwork']);
   }
 }
