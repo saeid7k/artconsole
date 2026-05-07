200172a49ec5fe611c608cbce869b0d82bddae51
Saeid Khajevand <saeid.kh7@gmail.com>
2026-05-06 23:51:24 -0400

    Add labelClassName prop to Container component

--- resources/js/Components/Container.tsx
+++ resources/js/Components/Container.tsx
@@ -3,15 +3,16 @@ import { PropsWithChildren } from "react";
 type Props = PropsWithChildren<{
   label?: string
   className?: string
+  labelClassName?: string
   children: React.ReactNode
 }>
 
-function Container({ label, className, children }: Props) {
+function Container({ label, className, labelClassName, children }: Props) {
   return (
     <div
       className={`mt-3 flex flex-col bg-base border border-dashed border-purple-300 rounded-lg p-2 ${className}`}
     >
-      <small className="text-primary -translate-y-1">{label}</small>
+      {label && <small className={`text-primary -translate-y-1 ${labelClassName}`}>{label}</small>}
       <div>
         {children}
       </div>
