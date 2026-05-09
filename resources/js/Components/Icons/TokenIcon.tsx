31e5441e69fbfee86aa6f712b298514b75dde47a
Saeid Khajevand <saeid.kh7@gmail.com>
2026-05-09 12:49:25 -0400

    Add TokenIcon component to display TokenCircleIcon with specified size and color

---
+++ resources/js/Components/Icons/TokenIcon.tsx
@@ -0,0 +1,10 @@
+import { TokenCircleIcon } from "@hugeicons/core-free-icons"
+import { HugeiconsIcon } from "@hugeicons/react"
+
+function TokenIcon() {
+  return (
+    <HugeiconsIcon icon={TokenCircleIcon} size={20} className="text-yellow-600" />
+  )
+}
+
+export default TokenIcon
