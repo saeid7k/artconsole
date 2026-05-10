0e43a488fb1e9f9e0feb61b963ab8173d6d5f218
Saeid Khajevand <saeid.kh7@gmail.com>
2026-05-10 12:49:51 -0400

    Add size prop to ItemRow component and adjust styling for responsive sizes

--- resources/js/Components/Containers/ItemRow.tsx
+++ resources/js/Components/Containers/ItemRow.tsx
@@ -3,16 +3,25 @@ import { twMerge } from "tailwind-merge"
 
 type Props = {
   icon?: React.ReactNode,
+  size?: 'sm' | 'md' | 'lg',
   gradientBackground?: boolean,
   onClick?: () => void,
   children: React.ReactNode,
 }
 
-function ItemRow({icon, gradientBackground = false, onClick, children}: Props) {
+function ItemRow({icon, size = 'md', gradientBackground = false, onClick, children}: Props) {
+
+  const sizeClasses = {
+    sm: 'px-2 py-1',
+    md: 'px-3 py-2',
+    lg: 'px-4 py-3',
+  }
+
   return (
     <div
       className={twMerge(
-        "flex items-center gap-1 border px-3 py-2 rounded-lg transition-all duration-300 bg-base",
+        "flex items-center gap-1 border rounded-lg transition-all duration-300 bg-base min-w-max",
+        sizeClasses[size],
         gradientBackground && 'hover:bg-linear-45 hover:from-purple-500/10 hover:to-blue-500/10 dark:hover:from-purple-950 dark:hover:to-blue-950',
         onClick && 'cursor-pointer',
       )}
@@ -20,7 +29,7 @@ function ItemRow({icon, gradientBackground = false, onClick, children}: Props) {
     >
       {icon && (
         <>
-          {icon}
+          <div>{icon}</div>
           <div className="self-stretch">
             <Divider orientation="vertical" className="h-[100%]" />
           </div>
