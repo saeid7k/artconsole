b060a2372d824ff03344a7fbcfcc557cc9f5641e
Saeid Khajevand <saeid.kh7@gmail.com>
2026-05-10 12:51:06 -0400

    develop TokensTransactions component and add topup button

--- resources/js/Components/Tokens/TokensDrawer.tsx
+++ resources/js/Components/Tokens/TokensDrawer.tsx
@@ -1,6 +1,9 @@
 import { formatNumber } from "@/utils/formatHelper";
-import { usePage } from "@inertiajs/react";
-import { Divider, Drawer } from "antd";
+import { Coins02Icon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { router, usePage } from "@inertiajs/react";
+import { Button, Divider, Drawer } from "antd";
+import { useEffect } from "react";
 import Container from "../Containers/Container";
 import FlexBox from "../Containers/FlexBox";
 import TokenIcon from "../Icons/TokenIcon";
@@ -15,22 +18,38 @@ function TokensDrawer({ open, onClose }: Props) {
 
   const user = usePage()?.props?.auth?.user;
 
+  useEffect(() => {
+    if (open) {
+      router.reload({ only: ['auth.user'] });
+    }
+  }, [open]);
+
   return (
     <Drawer
       title="Tokens Balance"
       onClose={onClose}
       open={open}
+      destroyOnHidden
     >
-      <Container
-        label="Available"
-        contentClassName="font-mono text-2xl leading-none"
-        bordered={false}
-      >
-        <FlexBox alignItems="end" gap={2}>
-          {formatNumber(user?.token_balance)}
-          <TokenIcon />
-        </FlexBox>
-      </Container>
+      <FlexBox justifyContent="between" >
+        <Container
+          label="Available"
+          contentClassName="font-mono text-2xl leading-none"
+          bordered={false}
+        >
+          <FlexBox alignItems="end" gap={2}>
+            {formatNumber(user?.token_balance)}
+            <TokenIcon />
+          </FlexBox>
+        </Container>
+        <Button
+          variant="outlined"
+          color="green"
+          icon={<HugeiconsIcon icon={Coins02Icon} size={16} />}
+        >
+          Top Up
+        </Button>
+      </FlexBox>
       <Divider className="text-ghost" >Transactions</Divider>
       <TokensTransactions />
     </Drawer>
--- resources/js/Components/Tokens/TokensTransactions.tsx
+++ resources/js/Components/Tokens/TokensTransactions.tsx
@@ -1,11 +1,19 @@
+import { TOKEN_TRANSACTION_TYPES } from "@/constants/tokenTransactionTypes";
+import { TokenTransaction } from "@/types/tokenTransaction";
+import { DownloadSquare01Icon, UploadSquare01Icon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
 import { useQuery } from "@tanstack/react-query";
+import { Button } from "antd";
 import axios from "axios";
 import { useState } from "react";
+import FlexBox from "../Containers/FlexBox";
 import ItemRow from "../Containers/ItemRow";
+import InfoPopover from "../InfoPopover";
+import StyledDate from "../StyledDate";
 
 function TokensTransactions() {
 
-  const [transactions, setTransactions] = useState<any[]>([]);
+  const [transactions, setTransactions] = useState<TokenTransaction[]>([]);
   const [transactionsUrl, setTransactionsUrl] = useState(route('tokens.transactions'));
 
   const transactionsQuery = useQuery({
@@ -13,22 +21,64 @@ function TokensTransactions() {
     queryFn: () => axios.get(transactionsUrl).then(res => {
       setTransactions(prev => [...prev, ...res.data.data]);
       setTransactionsUrl(res.data.next_page_url);
+      return res.data;
     }),
-    enabled: true,
+    enabled: !!transactionsUrl ? true : false,
     retry: false,
   });
 
+  const colorClass = (type: string) => {
+    switch (type) {
+      case 'top_up':
+      case 'credit':
+      case 'refund':
+        return 'text-green-600';
+      case 'usage':
+        return 'text-red-600';
+      default:
+        return '';
+    }
+  }
+
+  const getIcon = (type: string) => {
+    switch (type) {
+      case 'usage':
+        return <HugeiconsIcon icon={DownloadSquare01Icon} size={20} className="text-red-600" />;
+      default:
+        return <HugeiconsIcon icon={UploadSquare01Icon} size={20} className="text-green-600" />;
+    }
+  }
+
   return (
-    <div className="flex flex-col">
-      {transactions.map((item: any) => (
-        <ItemRow key={item.id}>
-          {item.description} - {item.amount}
+    <div className="flex flex-col gap-2 max-h-[300px] overflow-y-auto">
+      {transactions.map((item: TokenTransaction) => (
+        <ItemRow
+          key={item.id}
+          size="sm"
+        >
+          <FlexBox gap={2} justifyContent="between">
+            <FlexBox gap={2}>
+              {getIcon(item.type)}
+              <div className={`font-mono ${colorClass(item.type)}`}>{item.amount}</div>
+              <div>{TOKEN_TRANSACTION_TYPES[item.type]}</div>
+              {item.description && (
+                <InfoPopover
+                  content={item.description}
+                />
+              )}
+            </FlexBox>
+            <StyledDate value={item.created_at} showIcon={false} />
+          </FlexBox>
         </ItemRow>
       ))}
-
-      <pre>
-        {JSON.stringify(transactions, null, 2)}
-      </pre>
+      <Button
+        onClick={() => transactionsQuery.refetch()}
+        disabled={!transactionsUrl}
+        loading={transactionsQuery.isFetching}
+        className="shrink-0"
+      >
+        Load More
+      </Button>
     </div>
   );
 }
