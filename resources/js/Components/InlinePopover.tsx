37a7689627b564522c358ffaafc04fee5abb71dd
Saeid Khajevand <saeid.kh7@gmail.com>
2026-05-15 18:51:01 -0400

    feat: add placement and trigger props to InlinePopover component

--- resources/js/Components/InlinePopover.tsx
+++ resources/js/Components/InlinePopover.tsx
@@ -3,14 +3,18 @@ import { Popover } from "antd";
 type Props = {
   title?: string;
   content: string | React.ReactNode;
+  placement?: "top" | "left" | "right" | "bottom";
+  trigger?: "click" | "hover" | "focus";
   children: string | React.ReactNode;
 }
 
-function InlinePopover({ title, content, children }: Props) {
+function InlinePopover({ title, content, placement = 'bottom', trigger = 'click', children }: Props) {
   return (
     <Popover
       title={title}
       content={content}
+      placement={placement}
+      trigger={[trigger]}
     >
       <span className="border-b border-dashed border-muted mx-1 cursor-help">
         {children}
