0f4cec6c6c5370493d2f7f7bc09e25fddb47e457
Saeid Khajevand <saeid.kh7@gmail.com>
2026-05-16 08:15:30 -0400

    feat: update RecentArtworksWidget to include limit in API call and add View All button

--- resources/js/Components/Dashboard/RecentArtworksWidget.tsx
+++ resources/js/Components/Dashboard/RecentArtworksWidget.tsx
@@ -1,14 +1,15 @@
 import { ArtworkProps } from "@/types/artwork";
 import { useQuery } from "@tanstack/react-query";
-import { Card } from "antd";
+import { Button, Card } from "antd";
 import axios from "axios";
 import ArtworkStack from "../Artworks/ArtworkStack";
+import { router } from "@inertiajs/react";
 
 function RecentArtworksWidget() {
 
   const { data, isLoading } = useQuery({
     queryKey: ['recent-artworks'],
-    queryFn: () => axios.post(route('artworks.get-recent')).then(res => res.data),
+    queryFn: () => axios.post(route('artworks.get-recent'), { limit: 3 }).then(res => res.data),
     staleTime: Infinity
   });
 
@@ -16,6 +17,16 @@ function RecentArtworksWidget() {
     <Card
       title="Recent Artworks"
       loading={isLoading}
+      extra={
+        data && data.length > 0 && (
+          <Button type="link" size="small"
+            onClick={() => router.visit(route('artworks.index'))}
+          >
+            View All
+          </Button>
+        )
+      }
+      className={isLoading ? 'h-[295px]' : ''}
     >
       <div className="flex flex-col gap-2">
         {data && data.length > 0 && data.map((artwork: ArtworkProps) => (
@@ -23,6 +34,9 @@ function RecentArtworksWidget() {
             key={artwork.id}
             artwork={artwork}
             size="sm"
+            variant="outline"
+            rounded
+            onClick={() => router.visit(route('artworks.show', artwork.id))}
           />
         ))}
       </div>
