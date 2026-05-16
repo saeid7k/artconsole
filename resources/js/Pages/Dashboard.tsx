274d60766be91bb17b41a70d65095b68030f1022
Saeid Khajevand <saeid.kh7@gmail.com>
2026-05-16 12:33:27 -0400

    feat: add topSellingArtists method to ContactController and corresponding route

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -23,7 +23,7 @@ function Dashboard({}) {
           <ArtworksStatusWidget />
           <OwnershipWidget />
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-3">
+        <div className="grid grid-cols-1 xl:grid-cols-3 gap-3">
           <RecentArtworksWidget />
         </div>
       </div>
