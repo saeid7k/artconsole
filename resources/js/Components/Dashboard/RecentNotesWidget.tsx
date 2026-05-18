fb3e4d1de891c28e55a187a85f44fec7064ca187
Saeid Khajevand <saeid.kh7@gmail.com>
2026-05-17 20:53:25 -0400

    Add RecentNotesWidget component to Dashboard and update layout

---
+++ resources/js/Components/Dashboard/RecentNotesWidget.tsx
@@ -0,0 +1,50 @@
+import { NoteProps } from "@/types/note";
+import { router } from "@inertiajs/react";
+import { useQuery } from "@tanstack/react-query";
+import { Card, Empty } from "antd";
+import axios from "axios";
+import Note from "../Notes/Note";
+
+function RecentNotesWidget() {
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['recent-notes'],
+    queryFn: () => axios.get(route('notes.recent')).then(res => res.data.notes),
+    staleTime: Infinity
+  });
+
+  return (
+    <Card
+      title="Recent Notes"
+      loading={isLoading}
+      className='min-h-[240px]'
+      >
+      {
+        data?.length == 0 && <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="No notes found" />
+      }
+
+      <div className="flex flex-col gap-2"
+        style={{
+          maxHeight: '400px',
+          overflowY: 'auto',
+          scrollbarWidth: 'none',
+        }}
+      >
+        {data && data.length > 0 && data.map((note: NoteProps) => (
+          <Note
+            key={note.id}
+            note={note}
+            index={note.id}
+            rows={2}
+            deletable={false}
+            readonly={true}
+            readonlyClassName="line-clamp-2"
+            onClickRoot={note.noteable_path ? () => router.visit(note.noteable_path ?? '#') : undefined}
+          />
+        ))}
+      </div>
+    </Card>
+  )
+}
+
+export default RecentNotesWidget;
