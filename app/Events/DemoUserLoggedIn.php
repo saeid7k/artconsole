70cc27621b5b0ca6ef1b7e71fce9712d22aa51a6
Saeid Khajevand <saeid.kh7@gmail.com>
2026-08-03 21:46:56 -0400

    remove unused broadcasting methods in DemoUserLoggedIn event class

--- app/Events/DemoUserLoggedIn.php
+++ app/Events/DemoUserLoggedIn.php
@@ -3,11 +3,7 @@
 namespace App\Events;
 
 use App\Models\User;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
@@ -20,19 +16,5 @@ class DemoUserLoggedIn implements ShouldDispatchAfterCommit
    * Create a new event instance.
    */
   public function __construct(public User $user)
-  {
-    //
-  }
-
-  /**
-   * Get the channels the event should broadcast on.
-   *
-   * @return array<int, Channel>
-   */
-  public function broadcastOn(): array
-  {
-    return [
-      new PrivateChannel('channel-name'),
-    ];
-  }
+  {}
 }
